05bf6f0d5b3e630ce959f6da2cfb8a14c206c7b1
dodo <132376178+ldh-dodo@users.noreply.github.com>
2024-06-26 18:12:07 +0900

    [level 1] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,14 @@
+function solution(cards1, cards2, goal) {
+    var answer = '';
+    for(let i = 0; i < goal.length; i++){
+        if(cards1.length > 0 && cards1[0] === goal[i]){
+            cards1.shift();
+        } else if(cards2.length > 0 && cards2[0] === goal[i]){
+            cards2.shift();
+        } else {
+            return "No";
+        }
+    }
+    return "Yes";
+    return answer;
+}
