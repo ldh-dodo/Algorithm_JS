13d7509e246d3a9313ed16ae5757914f4b81892f
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-04-14 14:50:56 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 0.65 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.js
@@ -0,0 +1,94 @@
+function solution(beginning, target) {
+    const rowLen = beginning.length;
+    const colLen = beginning[0].length;
+    let [res1, res2, res3, res4] = [0, 0, 0, 0];
+    let flagArr = Array.from({length : rowLen}, () => Array.from({length : colLen}, () => 0));
+    // beginning과 target이 서로 다른 요소는 1, 같은 요소는 0으로.
+    for(let i = 0; i < rowLen; i++) {
+        for(let j = 0; j < colLen; j++) {
+            if(beginning[i][j] !== target[i][j]) {
+                flagArr[i][j] = 1;
+            }
+        }
+    }
+
+    // 행 다른 것 -> 열 다른 것
+    let copyFlag = JSON.parse(JSON.stringify(flagArr));
+
+    for(let i = 0; i < rowLen; i++) {
+        if(copyFlag[i][0] === 0) continue;
+
+        res1++;
+
+        for(let j = 0; j < colLen; j++) {
+            copyFlag[i][j] = 1 ^ copyFlag[i][j];
+        }
+    }
+    
+    for(let j = 0; j < colLen; j++) {
+        if(copyFlag[0][j] === 0) continue;
+
+        res1++;
+
+        for(let i = 0; i < rowLen; i++) {
+            copyFlag[i][j] = 1 ^ copyFlag[i][j];
+        }
+    }
+
+    let isSolved1 = copyFlag.every((row) => row.every((flag) => flag === 0));
+
+    // 열 다른 것 -> 열 다른 것
+    copyFlag = JSON.parse(JSON.stringify(flagArr));
+
+    for(let j = 0; j < colLen; j++) {
+        if(copyFlag[0][j] === 0) continue;
+
+        res2++;
+
+        for(let i = 0; i < rowLen; i++) {
+            copyFlag[i][j] = 1 ^ copyFlag[i][j];
+        }
+    }
+    for(let i = 0; i < rowLen; i++) {
+        if(copyFlag[i][0] === 0) continue;
+
+        res2++;
+
+        for(let j = 0; j < colLen; j++) {
+            copyFlag[i][j] = 1 ^ copyFlag[i][j];
+        }
+    }
+
+    const isSolved2 = copyFlag.every((row) => row.every((flag) => flag === 0));
+
+    // 행 안다른 것 -> 열 다른 것
+    copyFlag = JSON.parse(JSON.stringify(flagArr));
+
+    for(let i = 0; i < rowLen; i++) {
+        if(copyFlag[i][0] === 1) continue;
+
+        res3++;
+
+        for(let j = 0; j < colLen; j++) {
+            copyFlag[i][j] = 1 ^ copyFlag[i][j];
+        }
+    }
+
+    for(let j = 0; j < colLen; j++) {
+        if(copyFlag[0][j] === 0) continue;
+
+        res3++;
+
+        for(let i = 0; i < rowLen; i++) {
+            copyFlag[i][j] = 1 ^ copyFlag[i][j];
+        }
+    }
+
+    const isSolved3 = copyFlag.every((row) => row.every((flag) => flag === 0));
+    
+     if(isSolved1 && isSolved2 && isSolved3) return Math.min(res1, res2, res3);
+     else if(isSolved1) return res1;
+     else if(isSolved2) return res2;
+    else if(isSolved3) return res3;
+     else return -1;
+}
