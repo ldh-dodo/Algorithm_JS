7c8f0b7080200d10247cc1295f18c6d68363cd5f
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-04-24 15:03:02 +0900

    [level 3] Title: 정수 삼각형, Time: 25.52 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,24 +1,19 @@
 function solution(triangle) {
-    let answer = 0;
-    let dp = [[...triangle[0]]];
+    const SIZE = triangle.length;
+
+    let dp = Array.from({length : SIZE}, () => Array.from({length : SIZE}, () => 0));
     
+    dp[0][0] = triangle[0][0];
+    dp[1][0] = dp[0][0] + triangle[1][0];
+    dp[1][1] = dp[0][0] + triangle[1][1];
     
-    for(let i = 1; i < triangle.length; i++){
-        let row = [];
-        
-        for(let j = 0; j < triangle[i].length; j++){
-            let [leftDpValue, rightDpValue] = [dp[i-1][j-1], dp[i-1][j]];
-            if(leftDpValue && rightDpValue){
-                row.push(Math.max(leftDpValue, rightDpValue) + triangle[i][j]);    
-            } else if(leftDpValue){
-                row.push(leftDpValue + triangle[i][j]);
-            } else {
-                row.push(rightDpValue + triangle[i][j]);
-            }
+    for(let i = 2; i < SIZE; i++) {
+        for(let j = 0; j < triangle[i].length; j++) {
+            // 다음 dp = 1. 기존 저장된 dp 값, 2. 이전 dp + 좌상단 triangle 값 3. 이전 dp + 우상단 triangle 값
+            let max = Math.max((dp[i - 1][j - 1] | -Infinity), (dp[i - 1][j] | -Infinity));
+            dp[i][j] = Math.max(dp[i][j], triangle[i][j] + max);
         }
-        dp.push(row);
     }
     
-    answer = Math.max(...dp[dp.length-1]);
-    return answer;
+    return Math.max(...dp[SIZE - 1]);
 }
