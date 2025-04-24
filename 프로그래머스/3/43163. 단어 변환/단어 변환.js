24a6043cdde24b1b32d06f08be752ef33e3f2e54
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-04-24 16:46:53 +0900

    [level 3] Title: 단어 변환, Time: 0.98 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,50 +1,46 @@
-function canConvert(source, target){
-    let len = source.length;
-    
-    let cnt = 0;
-    for(let i = 0; i < len; i++){
-        if(cnt >= 2) return false;
-        
-        if(source[i] !== target[i]) cnt++;
-    }
-    
-    if(cnt === 1) return true;
-    
-    return false;
-}
 function solution(begin, target, words) {
-    /*
-    words의 단어를 이용해,
-    begin -> target으로 가는 가장 짧은 변환 과정 찾기
+    words.push(begin);
+    const SIZE = words.length;
+    let graph = Array.from({length : SIZE }, () => []);
+    let [beginIdx, targetIdx] = [SIZE - 1, null];
+    let visited = Array.from({length : SIZE }, () => false);
     
-    출발지와 목적지가 정해진 BFS
-    */
-    if(!words.includes(target)) return 0;
+    if(!words.some((word) => word === target)) return 0;
     
-    let q = [];
-    let checked = {};
-    
-    q.push(begin);
-    checked[begin] = 0; // 초기 단계는 0단계. checked hash에 값이 존재한다면 방문한 것
-    
-    while(q.length > 0){
-        let cur = q.pop();
+    for(let i = 0; i < SIZE; i++) {
+        const cur = words[i];
+        if(cur === target) targetIdx = i;
         
-        if(cur === target) break;
-        
-        for(const next of words){
-            // 1. next가 방문 되었는지 검사
-            // 2. 1 통과시, cur과 한글자만 다른지 검사
-            // 3. 2 통과시, 방문 체크 후, 큐에 삽입
+        for(let j = 0; j < SIZE; j++) {
+            if(i === j) continue;
             
-            if(checked[next]) continue;
-            if(!canConvert(cur, next)) continue;
+            const next = words[j];
+            let cnt = 0;
             
-            checked[next] = checked[cur] + 1;
-            q.push(next);
+            for(let k = 0; k < cur.length; k++) {
+                if(cnt > 1) break;
+                
+                if(cur[k] !== next[k]) cnt++;
+            }
+            
+            if(cnt === 1) graph[i].push(j);
         }
     }
-    
-    if(checked[target]) return checked[target];
-    else return 0;
+
+     let q = [[beginIdx, 0]];
+   
+     visited[beginIdx] = true;
+
+    while(q.length > 0) {
+        const [curIdx, cnt] = q.shift();
+        visited[curIdx] = true;
+        
+        if(curIdx === targetIdx) return cnt;
+        
+        for(const nextIdx of graph[curIdx]) {
+            if(visited[nextIdx]) continue;
+            
+            q.push([nextIdx, cnt + 1]);
+        }
+    }
 }
