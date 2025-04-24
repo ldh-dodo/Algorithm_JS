a8d91c940eeba0e1969db900ac27c75315d7f75f
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-04-24 14:24:18 +0900

    [level 3] Title: 네트워크, Time: 0.55 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,34 +1,35 @@
 function solution(n, computers) {
-    let graph = Array.from({length : n}, () => []);
-    let visited = Array.from({length : n}, () => false);
-    let cnt = 0;
+    let answer = 0;
+    let len = computers.length;
+    let q = [];
     
-    computers.forEach((computer, i) => {
-        computer.forEach((target, j) => {
-            if(i === j || target === 0) return;
-        
-            graph[i].push(j);
-        });
-    });
+    let checked = new Array(len).fill(false);
     
-    for(let i = 0; i < n; i++) {
-        if(visited[i]) continue;
+    for(let i = 0; i < len; i++){
+        if(checked[i]) continue; // 방문했다면 다음 노드 탐색
         
-        cnt++;
-        let q = [i];
-        visited[i] = true;
+        // 방문 안했다면 인덱스를 큐에 push 후 방문 검사, 이후 BFS.
+        // 그리고 하나의 연결된 네트워크를 검사할테니, answer 1 증가
         
-        while(q.length > 0) {
-            const cur = q.shift();
+        q.push(i);
+        checked[i] = true;
+        answer++;
+        
+        while(q.length > 0){ // 큐가 비어있지 않을 때 동안 반복
+            let j = q.pop();
             
-            for(const next of graph[cur]) {
-                if(visited[next]) continue;
+            // idx가 가리키는 컴퓨터가 가지고 있는 연결된 네트워크 정보를 순회
+            // 예를들어 idx가 0이라면, computers[0]에 연결된 노드를 체크하면서 순회한다
+            for(let idx = 0; idx < len; idx++){
+                if(j === idx  || checked[idx] || !computers[j][idx]) continue;
+                // 자기 자신이거나, 방문했다면 pass
                 
-                visited[next] = true;
-                q.push(next);
+                // 아니라면 큐에 넣고 방문 체크
+                q.push(idx);
+                checked[idx] = true;
             }
         }
     }
     
-    return cnt;
+    return answer;
 }
