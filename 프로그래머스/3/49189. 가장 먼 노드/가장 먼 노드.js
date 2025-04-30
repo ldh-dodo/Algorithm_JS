01c5688858958d4be39d6f60a2ba17f419ddad23
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-04-30 15:13:35 +0900

    [level 3] Title: 가장 먼 노드, Time: 48.68 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,46 +1,38 @@
 function solution(n, edge) {
-    /*
-    요구사항
-    1. 노드는 1부터 n까지 번호가 적혀있음
-    2. 1번 노드에서 가장 멀리 떨어진 노드의 '갯수'를 구하기
-    3. 각 노드간의 거리는 1
-    */
+    // node : 1-based
+    // 1번 노드에서 최단 경로 기준가장 멀리 떨어진 노드의 갯수
+        
     let answer = 0;
-    let dist = new Array(n).fill(null);
+    let graph = new Array(n + 1).fill().map(() => []);
+    let visited = new Array(n + 1).fill(false);
     let q = [];
     
-    let graph = new Array(n).fill(null).map(() => []);
-
-    edge.forEach((item) => {
-        graph[item[0]-1].push(item[1]-1);
-        graph[item[1]-1].push(item[0]-1);
-    }) 
-    console.log(graph);
-    
-    // 거리 초기화 및 큐에 처음 노드 삽입
-    dist[0] = 0;
-    graph[0].forEach((node) => {
-        dist[node] = 1;
-        q.push(node);
-    })
-    
-    let max = 0;
-    
-    while(q.length > 0){
-        const node = q.shift();
-        graph[node].forEach((adj_node) => {
-            if(dist[adj_node] !== null) return;
-            
-            dist[adj_node] = dist[node] + 1;
-            q.push(adj_node);
-            
-            if(dist[adj_node] > max) {
-                max = dist[adj_node];
-            }
-        });
+    for(const [u, v] of edge) {
+        graph[u].push(v);
+        graph[v].push(u);
     }
     
-    const max_dist = dist.filter((dist_node) =>  dist_node === max);
+    q = [[1, 0]]; // [node, dist];
+    visited[1] = true;
+    let maxDist = -Infinity;
+    
+    while(q.length > 0) {
+        const [node, dist] = q.shift();
 
-    return max_dist.length;
+        if(dist > maxDist) {
+            maxDist = dist;
+            answer = 1;
+        } else {
+            answer++;
+        }
+        
+        for(const neighbor of graph[node]) {
+            if(visited[neighbor]) continue;
+            
+            visited[neighbor] = true;
+            q.push([neighbor, dist + 1]);
+        }
+    }
+    
+    return answer;
 }
