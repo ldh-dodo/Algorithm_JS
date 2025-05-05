009ff42185dd5fd21839466a89f7abab8a47b96c
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-05-05 13:41:03 +0900

    [level 3] Title: 징검다리 건너기, Time: 45.50 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -1,27 +1,28 @@
-
 function solution(stones, k) {
     let left = 1;
     let right = 200000000;
     
-    while(left <= right){
-        let mid = Math.floor((left+right) / 2);
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
         
         let cnt = 0;
         
-        for(let i = 0; i < stones.length; i++){
-            if(stones[i] - mid <= 0){
+        for(let i = 0; i < stones.length; i++) {
+            if(mid >= stones[i]) {
                 cnt++;
             } else {
                 cnt = 0;
             }
-            if(cnt === k) break;
+            
+            if(cnt >= k) break;
         }
         
-        if(cnt !== k){
-            left = mid + 1;
-        } else {
+        if(cnt >= k) {
             right = mid - 1;
+        } else {
+            left = mid + 1;
         }
     }
+    
     return left;
 }
