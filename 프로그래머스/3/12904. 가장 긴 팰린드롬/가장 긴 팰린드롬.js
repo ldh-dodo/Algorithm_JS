78d60e82f34c1c3cca885a886261af4c5e2038be
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-05-20 13:58:35 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 13.00 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.js
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.js
@@ -1,34 +1,33 @@
 function solution(s)
 {
-    // 팰린드롬 : 앞뒤를 뒤집어도 똑같음
-    // 부분 문자열 중 가장 긴 팰린드롬의 길이를 리턴
-    
     /*
-        가장 긴 문자열부터 팰린드롬 찾아서 바로 반환
-        
-        
+        부분 문자열의 팰린드롬을 구하라
+    1. 모든 요소를 중심으로 삼는다.
+    2. 중심으로부터 왼쪽 오른쪽을 비교해나가며, 팰린드롬의 최대 길이를 찾는다.
     */
+
+    let answer = 1;
+    const len = s.length;
     
-    let answer = null;
-    const n = s.length;
-    
-    for(let i = n; i > 1; i--) { // i 길이만큼 잘라서 비교
-        for(let j = 0; j + i <= n; j++) {
-            let flag = true;
-            for(let k = 0; k < Math.floor(i / 2); k++) {
-                const st = j + k;
-                const end = j + i - 1 - k;
-                
-                if(s[st] !== s[end]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) {
-                return i;
-            }   
+    for(let i = 0; i < len; i++) {
+        let left = i - 1;
+        let right = i + 1;
+
+        while(left >= 0 && right < len && s[left] === s[right]) {
+            answer = Math.max(answer, (right - left + 1));
+            left--;
+            right++;
+        }
+        
+        left = i;
+        right = i + 1;
+        
+        while(left >= 0 && right < len && s[left] === s[right]) {
+            answer = Math.max(answer, (right - left + 1));
+            left--;
+            right++;
         }
     }
     
-    return 1;
+    return answer;
 }
