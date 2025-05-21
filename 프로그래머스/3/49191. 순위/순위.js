4e904240a0ae5200d2f7cee8d5b1f094210ca17b
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-05-21 14:58:44 +0900

    [level 3] Title: 순위, Time: 10.96 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,33 +1,37 @@
 function solution(n, results) {
-    let answer = 0;
+    /*
+    1 ~ n 번 권투 선수
+    권투는 1 대 1
+    실력이 좋다면 반드시 이김
     
-    let floydArr = Array(n + 1).fill(0).map(() => new Array(n + 1).fill(false));
-
-    results.forEach(([winner, loser]) => {
-        floydArr[winner][loser] = true; 
-    });
+    경기 결과가 분실되었고, 정확하게 순위를 매길 수 있는 선수의 수를 반환
     
-    for(let k = 1; k <= n; k++) {
-        for(let winner = 1; winner <= n; winner++) {
-            for(let loser = 1; loser <= n; loser++) {
-                if(floydArr[winner][loser] ||
-                (floydArr[winner][k] && floydArr[k][loser])) {
-                    floydArr[winner][loser] = true;
-                }
-            }
-         }
+    특정 노드에서 출발해서 목표 노드까지 도달할 수 있는 경우가 n - 1 이라면, 결과를 도출할 수 있음
+    */
+    
+    let floyd = new Array(n + 1).fill().map(() => new Array(n + 1).fill(false));
+    let checked = new Array(n + 1).fill(0);
+    
+    for(const [u, v] of results) {
+        floyd[u][v] = true;
     }
-    
-    let checked = Array(n + 1).fill(0);
-    
-    for(let i = 1; i <= n; i++) {
-        for(let j = 1; j <= n; j++) {
-            if(floydArr[i][j]) {
-                checked[i]++;
-                checked[j]++;
+
+    for(let k = 1; k <= n; k++) {
+        for(let i = 1; i <= n; i++) {
+            for(let j = 1; j <= n; j++) {
+                if(floyd[i][k] && floyd[k][j]) floyd[i][j] = true;
             }
         }
     }
     
-    return checked.filter((item) => item === n - 1).length;
+   for(let i = 1; i <= n; i++) {
+       for(let j = 1; j <= n; j++) {
+           if(floyd[i][j]) {
+               checked[i] += 1;
+               checked[j] += 1;
+           }
+        }
+   }
+    
+    return checked.reduce((acc, cur) => (cur === n - 1 ? acc + 1 : acc), 0);
 }
