d478993360a3c07f06b6e84def049903633862da
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-05-22 16:41:08 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 119.26 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
@@ -1,48 +1,54 @@
-function solution(board, skill) { 
-    function makePrefixSumArr() {
-        let rLen = board.length;
-        let cLen = board[0].length;
-        
-        let prefixSumArr = Array(rLen + 1).fill(0).map(() => Array(cLen + 1).fill(0));
-        
-     
-        skill.forEach(([type, r1, c1, r2, c2, degree]) => {
-            if(type === 1) degree = -degree;
-            
-            prefixSumArr[r1][c1] += degree;
-            prefixSumArr[r1][c2 + 1] -= degree;
-            prefixSumArr[r2 + 1][c1] -= degree;
-            prefixSumArr[r2 + 1][c2 + 1] += degree;
-
-        });
-        
-        for(let i = 0; i < rLen + 1; i++) {
-            for(let j = 1; j < cLen + 1; j++) {
-                prefixSumArr[i][j] += prefixSumArr[i][j - 1];
-            }
-        }
-        
-        for(let j = 0; j < cLen + 1; j++) {
-            for(let i = 1; i < rLen + 1; i++) {
-                prefixSumArr[i][j] += prefixSumArr[i - 1][j];
-            }
-        }
-        
-        return prefixSumArr;
-    }
+function solution(board, skill) {
+    /*
+    각 건물이 내구도를 가짐
+    
+    공격과 회복은 항상 직사각형
+    
+    skill
+    [type, r1, c1, r2, c2, degree]
+    type 1 : 공격 2 : 회복
+    
+    1. 크기가 1씩 더 큰 부분합 배열을 만든다.
+    2. 각 skill마다 부분합 배열의 아래 좌표에 degree 만큼을 더한다.
+    (r1, c1) -> degree, (r1, c2 + 1) -> -degree, (r2 + 1, c1) -> -degree, (r2 + 1, c2 + 1) -> degree
+    3. 부분합 배열에서 부분합을 구한다.
+    4. 테두리를 제외한 부분을 순회하며, 파괴되지 않은 건물의 수를 구한다.
+    */
     
     let answer = 0;
+    let rLen = board.length;
+    let cLen = board[0].length;
     
-    let prefixSumArr = makePrefixSumArr();
+    let prefixSum = new Array(rLen + 1).fill().map(() => new Array(cLen + 1).fill(0));
     
+    for(const [type, r1, c1, r2, c2, degree] of skill) {
+        const cost = type === 1 ? -1 * degree : degree;
+        
+        prefixSum[r1][c1] += cost;
+        prefixSum[r1][c2 + 1] += (-cost);
+        prefixSum[r2 + 1][c2 + 1] += cost;
+        prefixSum[r2 + 1][c1] += (-cost);
+    }
     
-    for(let i = 0; i < board.length; i++) {
-        for(let j = 0; j < board[0].length; j++) {
-            board[i][j] += prefixSumArr[i][j];
-            
-            if(board[i][j] > 0) answer++;
+    // 부분합 계산
+    for(let r = 0; r < rLen; r++) {
+        for(let c = 1; c < cLen; c++) {
+            prefixSum[r][c] += prefixSum[r][c - 1]
         }
     }
     
+    for(let c = 0; c < cLen; c++) {
+        for(let r = 1; r < rLen; r++) {
+            prefixSum[r][c] += prefixSum[r - 1][c];
+        }
+    }
+    
+    for(let r = 0; r < rLen; r++) {
+        for(let c = 0; c < cLen; c++) {
+            board[r][c] += prefixSum[r][c];
+            if(board[r][c] > 0) answer++; 
+        }
+    }
+
     return answer;
 }
