12837b7f8a2a0d23b87a69345b29c0cd773d0bd6
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-05-26 14:35:00 +0900

    [level 3] Title: 표 편집, Time: 402.27 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.js
+++ 프로그래머스/3/81303. 표 편집/표 편집.js
@@ -1,65 +1,65 @@
-
-class Node {
-    constructor(cur, prev, next) {
-        this.cur = cur;
-        this.prev = prev;
-        this.next = next;
-    }
-}
-
 function solution(n, k, cmd) {
     /*
-    "U X" : 선택된 행 X칸 위 행 선택
-    "D X" : 선택된 행 X칸 아래 행 선택
-    "C" : 1. 현재 선택된 행 삭제 2. 바로 아래 행 선택(마지막 행이면 바로 위 행 선택)
-    "Z" : 가장 최근에 삭제된 행을 원래대로 복구. 선택된 행 변경 X
+    U X : 현재 선택 행에서 X칸 Up
+    D X : 현재 선택 행에서 X칸 Down
+    C : 현재 선택 행 삭제, 바로 아래 행 선택(마지막이면 바로 위)
+    Z : 가장 최근에 삭제된 행을 원래대로 복구(선택된 행 그대로)
     */
     
-    // 각 노드는 [자신의 왼쪽 노드 인덱스, 자신의 오른쪽 노드 인덱스] 를 가진다.
-    let nodes = Array(n);
-    let st = [];
+    class Node {
+        constructor(cur, prev, next) {
+            this.cur = cur;
+            this.prev = prev;
+            this.next = next;
+        }
+        
+
+    }
+    
+    const list = [];
+    const st = [];
     
     for(let i = 0; i < n; i++) {
-        nodes[i] = new Node(i, i - 1, i + 1);
+        const prev = i - 1 < 0 ? null : i - 1;
+        const next = i + 1 >= n ? null : i + 1;
+        list.push(new Node(i, prev, next));        
     }
-    nodes[0].prev = null
-    nodes[n - 1].next = null
     
-    cmd.forEach((command) => {
-        const [action, count] = command.split(' ');
+    for(const command of cmd) {
+        const [com, dist] = command.split(' ');
         
-        switch(action) {
-            case 'U' :
-                for(let i = 0; i < Number(count); i++) k = nodes[k].prev;
+        switch(com) {
+            case 'U':
+                for(let i = 0; i < Number(dist); i++) k = list[k].prev;
                 break;
             case 'D' :
-                for(let i = 0; i < Number(count); i++) k = nodes[k].next;
+                for(let i = 0; i < Number(dist); i++) k = list[k].next;
                 break;
-            case 'C' :
+            case 'C':
                 st.push(k);
-                let currentNode = nodes[k];
-                let nextNode = currentNode.next !== null ? nodes[currentNode.next] : null
-                let prevNode = currentNode.prev !== null ? nodes[currentNode.prev] : null;
-                
-                if(prevNode !== null) prevNode.next = currentNode.next;
-                if(nextNode !== null) nextNode.prev = currentNode.prev;
+                let curNode = list[k];
+                let nextNode = curNode.next !== null ? list[curNode.next] : null;
+                let prevNode = curNode.prev !== null ? list[curNode.prev] : null;
 
-                k = currentNode.next !== null ? currentNode.next : currentNode.prev;
+                if(prevNode !== null) prevNode.next = curNode.next;
+                if(nextNode !== null) nextNode.prev = curNode.prev;
+
+                k = curNode.next !== null ? curNode.next : curNode.prev;
                 break;
             case 'Z' :
                 const rollbackIdx = st.pop();
-                let rollbackNode = nodes[rollbackIdx];
+                let rollbackNode = list[rollbackIdx];
                 let rollbackNextNode = 
-                    rollbackNode.next !== null ? nodes[rollbackNode.next] : null;
+                    rollbackNode.next !== null ? list[rollbackNode.next] : null;
                 let rollbackPrevNode = 
-                    rollbackNode.prev !== null ? nodes[rollbackNode.prev] : null;
+                    rollbackNode.prev !== null ? list[rollbackNode.prev] : null;
                 
                 if(rollbackNextNode) rollbackNextNode.prev = rollbackIdx;
-                if(rollbackPrevNode) rollbackPrevNode.next = rollbackIdx;       
-                break;           
+                if(rollbackPrevNode) rollbackPrevNode.next = rollbackIdx;
+                break;
         }
-    });
-
+    }
+    
     let answer = Array(n).fill('O');
     
     st.forEach((deletedIdx) => {
