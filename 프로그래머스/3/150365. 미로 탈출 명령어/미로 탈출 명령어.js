331c5fbde1bfe33cb58c976718a950cd53723e8b
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-06-04 13:10:20 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 23.77 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,65 +1,65 @@
-function solution(n, m, x, y, r, c, k) {
-    /* 사전순 d l r u 
-       (x,y) -> (r,c) 로 이동하는 거리가 k여야 함
-       같은 격자를 두 번 이상 방문해도 됨.
-       탈출 못할 경우 "impossible" 
-       
-       좌측 상단 (1,1)
-       우측 하단(n,m)
-       . : 빈 공간
-       S : 출발 지점
-       E : 탈출 지점
-    */ 
+function solution(n, m, x, y, targetX, targetY, targetDist) {
+    /*
+    격자 : n x m
+    목표 : (x, y) -> (r, c) 이동
+    
+    조건
+    1. 벽 바깥으로는 못나감
+    2. 탈출했을 때, 거리가 k여야 함.
+    3. 같은 격자를 두 번 이상 방문해도 됨
+    4. 탈출 경로를 문자열로 나타냈을 때, 사전순으로 가장 빠른 경로로 탈출
+    5. 경로가 존재하지 않을 수 있음
+    
+    
+    d, l, r, u 순으로 다음 이동 경로를 추적한다.
+    현재 거리 > k 라면 탐색 중지
+    현재 거리 = k 라면, 현재 위치가 목적지인지 확인 후, 그렇다면 바로 문자열로 반환한다.
+    
+    
+    */
+    
+    let answer = '';
+    
+    let dx = [1, 0, 0, -1];
+    let dy = [0, -1, 1, 0];
+    let pStr = ['d', 'l', 'r', 'u'];
+    const path = [];
+    
+    const res = dfs(x, y, targetDist) || 'impossible';
+    
+    return res;
+    
+    function dfs(curX, curY, movesLeft) {
+        if(movesLeft === 0) {
+            if(curX === targetX && curY === targetY) return path.join('');
+            else return null;
+        }
         
-    function canReach(curY, curX, movesLeft) {
-        const remainDist = Math.abs(curY - r) + Math.abs(curX - c);
+        if(!canReach(curX, curY, movesLeft)) return null;
+        
+        for(let i = 0; i < 4; i++) {
+            const [nx, ny] = [curX + dx[i], curY + dy[i]];
+            
+            if(ny <= 0 || nx <= 0 || ny > m || nx > n) continue;
+            
+            path.push(pStr[i]);
+            const res = dfs(nx, ny, movesLeft - 1);
+            
+            if(res !== null) return res;
+            path.pop();
+        }
+        
+    }
+    
+    function canReach(curX, curY, movesLeft) {
+        // 도달가능한지 판별법
+        // 남은 거리가 남은 이동 수보다 크면 도달 불가능
+        // 남은 거리가 남은 이동 수보다 작으면, 두 개를 뺀 값이 0포함 짝수여야, 되돌아와도 도착할 수 있음
+        const remainDist = Math.abs(curY - targetY) + Math.abs(curX - targetX);
         
         if(remainDist > movesLeft) return false;
-        if((remainDist - movesLeft) % 2 !== 0) return false;
+        if(Math.abs(remainDist - movesLeft) % 2 !== 0) return false;
         
         return true;
     }
-    
-    function dfs(curY, curX, movesLeft) {
-        if(movesLeft === 0) {
-            if(curY === r && curX === c) {
-                return path.join('');
-            }
-            
-            return null;
-        }
-        
-        if(!canReach(curY, curX, movesLeft)) return null;
-
-        for(const [dy, dx, pathStr] of dir) {
-            const ny = curY + dy;
-            const nx = curX + dx;
-            
-            if(ny <= 0 || ny > n || nx <= 0 || nx > m) continue;
-            
-            path.push(pathStr);
-            const res = dfs(ny, nx, movesLeft - 1);
-            
-            if(res !== null) return res;
-            
-            path.pop();
-        }
-    }
-
-    
-    const map = Array(n + 1).fill().map(() => Array(m + 1).fill(0));
-    const dir = [
-        [1,0, 'd'], // d
-        [0,-1, 'l'], // l
-        [0, 1, 'r'], // r
-        [-1, 0, 'u'] // u
-    ];
-    const path = [];
-    
-    map[x][y] = "S";
-    map[r][c] = "E";
-    
-    const answer = dfs(x, y, k) || "impossible";
-    
-    return answer;
 }
