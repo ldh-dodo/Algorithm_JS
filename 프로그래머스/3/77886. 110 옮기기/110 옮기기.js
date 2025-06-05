9e7e6a09e8ec9851f7fb27f56f060814f06c85d1
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-06-05 14:00:38 +0900

    [level 3] Title: 110 옮기기, Time: 93.66 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
@@ -1,38 +1,48 @@
-function solution(s) {
-    const answer = [];
-    const TARGET_STRING = '110';
+function solution(str) {
+    /*
+    문자열은 0과 1로만 이루어져 있다.
+    "110" 을 뽑아서 임의의 위치에 다시 삽입할 수 있다.
+    문자열을 최대한 사전순으로 앞에 오도록 만들어야 한다.
     
-    for(const str of s) {
-        const st = [];
+    풀이 전략
+
+    문자를 순회하는 동안, 스택에 담고, 110이 담겨있다면, 추출하는 방식으로 진행
+    추출하고 남은 문자열의 가장 마지막 0 뒤에 삽입하면 사전순으로 가장 앞에 올 수 있음
+    */
+    
+    let answer = [];
+    const STR_110 = '110';
+    
+    for(let s of str) {
         let cnt = 0;
+        let st = [];
         
-        for(const char of str) {
-            if(st.length >= 2 &&
-                st[st.length - 1] === '1' &&
-                st[st.length - 2] === '1' &&
-                char === '0') {
+        for(let i = 0; i < s.length; i++) {
+            st.push(s[i]);
+            
+            if(st.length < 3) continue;
+            
+            const len = st.length;
+                  
+            if(st[len - 1] === '0' && st[len - 2] === '1' && st[len - 3] === '1') {
+                cnt++;
                 
                 st.pop();
                 st.pop();
-                cnt++;
-                continue;
-            } 
-            st.push(char);
+                st.pop();
+            }
         }
         
-        let insertIdx = 0;
+        // 남은 st문자열에 가장 마지막 0 뒤에 cnt만큼 110을 삽입하면 됨
         
-        st.forEach((char, idx) => {
-            if(char === '0') insertIdx = idx + 1;
-        })
+        st = st.join('');
         
-        const res = 
-              st.slice(0, insertIdx).join('') + 
-              TARGET_STRING.repeat(cnt) +
-              st.slice(insertIdx).join('');
+        const lastIndex = st.lastIndexOf('0');
+        const result = st.slice(0, lastIndex + 1) + 
+              STR_110.repeat(cnt) + st.slice(lastIndex + 1);
         
-        answer.push(res);
+        answer.push(result);
     }
-    
+
     return answer;
 }
