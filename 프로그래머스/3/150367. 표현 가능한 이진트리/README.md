e525cacf8cb4494ada59db0dec7b84af37891a99
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-06-09 14:35:41 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 40.58 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/README.md
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/README.md
@@ -1,10 +1,10 @@
 # [level 3] 표현 가능한 이진트리 - 150367 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367) 
 
 ### 성능 요약
 
-메모리: 43.5 MB, 시간: 53.05 ms
+메모리: 39 MB, 시간: 40.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 18일 15:48:04
+2025년 06월 09일 14:35:36
 
 ### 문제 설명
 
--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.js
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.js
@@ -1,58 +1,54 @@
 function solution(numbers) {
-    let answer = [];
-    let parsedStrings = [];
+    /*
+    살펴본 노드가 더미 노드라면 문자열 뒤에 0을 추가한다.
+    살펴본 노드가 더미 노드가 아니라면 문자열 뒤에 0을 추가한다.
+    문자열에 저장된 이진수를 십진수로 변환한다.
     
-    for(const number of numbers) {
-        let parsed = number.toString(2);
-        let length = 2 ** (Math.floor(Math.log2(parsed.length)) + 1) - 1;
-            
-        parsed = parsed.padStart(length, "0");
-        parsedStrings.push(parsed);
+    수가 주어졌을 때 하나의 이진트리로 해당 수를 표현할 수 있는지를 판별
+    
+    풀이 전략
+    - 주어진 십진수를 이진수로 변환한 뒤, 이진트리로 표현할 수 있는지를 확인한다.
+    - 이진수 1 부분의 조건
+        - 부모가 1이어야 함.
+    - 이진수 0 부분의 조건
+        - 부모의 조건에 따라 상관이 없음
+        - 부모가 0이어야 함.
+    */
+    
+    const result = [];
+    
+    for(let number of numbers) {
+        number = number.toString(2);
+        
+        let length = 2 ** (Math.floor(Math.log2(number.length)) + 1) - 1; // 2^h - 1
+        number = number.padStart(length, "0");
+        
+        if(canParse(number, 0, number.length - 1)) result.push(1);
+        else result.push(0);    
     }
     
-    for (const parsedStr of parsedStrings) {
-        if(parsedStr.length === 1) {
-            answer.push(1);
-            continue;
-        }
+    function canParse(number, left, right) {
+        if(left === right) return true;
         
-        const len = parsedStr.length;
-
-        let canMakeBT = canMakeBinaryTree(0, len - 1, [...parsedStr]) ? 1 : 0;
-        
-        answer.push(canMakeBT);
-    }
-    
-    function canMakeBinaryTree(left, right, parsedArr) {
-        if(left === right) return true; // 자식 노드가 없다 -> return true;
-        
-        // 자식 노드가 있을 때
-    
         const mid = Math.floor((left + right) / 2);
+        const canParseLeft = canParse(number, left, mid - 1);
         
-        const isLeftCanMakeBT = canMakeBinaryTree(left, mid - 1, parsedArr);
-                
-        if(!isLeftCanMakeBT) return false;
+        if(!canParseLeft) return false;
         
-        const isRightCanMakeBT = canMakeBinaryTree(mid + 1, right, parsedArr);
+        const canParseRight = canParse(number, mid + 1, right);
         
-        if(!isRightCanMakeBT) return false;
+        if(!canParseRight) return false;
         
-        // leftHasOne || rightHasOne 이 true 인 경우
-            // 자신이 1면 true, 아니면 false
-        // false 인 경우
-            // return true
+        const leftChild = number[Math.floor((left + mid) / 2)];
+        const rightChild = number[Math.ceil((mid + right) / 2)];
         
-        const leftChildIdx = Math.floor((left + mid) / 2);
-        const rightChildIdx = Math.ceil((mid + right) / 2);
-        
-        if(parsedArr[leftChildIdx] === '1' || parsedArr[rightChildIdx] === '1') {
-            if(parsedArr[mid] === '1') return true;
+        if(leftChild === '1' || rightChild === '1') { // 자식이 1이라면, 자기 자신도 1이어야 함.
+            if(number[mid] === '1') return true;
             return false;
         }
         
         return true;
     }
     
-    return answer;
+    return result;
 }
