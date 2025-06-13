d583efb39cafe97cec4ada9da04dd2c604ab0cd1
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-06-13 13:18:32 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 29.63 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.js
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.js
@@ -1,36 +1,48 @@
 function solution(n, cores) {
+    /*
+    - 코어별로 작업을 처리하는 시간이 다름
+    - 한 코어가 작업이 끝나면 작업이 없는 코어가 다음 작업을 수행
+    - 앞의 코어부터 작업을 처리
+    
+    n : 작업의 개수
+    cores : 코어의 처리시간
+    
+    마지막 작업을 처리하는 코어의 번호를 반환하라.
+    
+    풀이 전략
+    코어를 처리하는 총 시간을 구하고, 총 시간을 통해 마지막 코어의 번호를 구하기
+    이분 탐색 활용
+    최대 시간 = 작업 처리 최대 시간 * 최대 일의 개수 = 10000 * 50000 = 500000000
+    */
+    
     let l = 0;
-    let r = 10000 * 50000 + 1;
+    let r = 500000001;
+    
+    let totalTime = null;
     
     while(l < r) {
-        let m = parseInt((l + r) / 2);
-      
-        const totalWorks = getTotalWorks(m);
-
-        if(totalWorks >= n) r = m;
-        else l = m + 1;
+        let mid = Math.floor((l + r) / 2);
+        
+        const workCnt = cores.reduce((acc, cur) => acc + Math.floor(mid / cur), 0) 
+                        + cores.length;    
+        
+        if(workCnt >= n) r = mid;
+        else l = mid + 1;
     }
     
-    let totalWorks = getTotalWorks(r);
-    let time = r;
+    let totalWorks = cores.reduce((acc, cur) => acc + Math.floor(r / cur), 0) 
+                        + cores.length;   
+    totalTime = r;
     
-    while(true) {
+    while(totalTime > 0) {
         for(let i = cores.length - 1; i >= 0; i--) {
-            if(time % cores[i] === 0) {
+            if(totalTime % cores[i] === 0) {
                 totalWorks--;
                 if(totalWorks === n - 1) {
                     return i + 1;
                 }
             }
         }
-        time--;
-    }
-
-    function getTotalWorks(hours) {
-        let totalWorks = 0;
-        for(const core of cores) {
-            totalWorks += Math.floor(hours / core) + 1;
-        }
-        return totalWorks;
+        totalTime--;
     }
 }
