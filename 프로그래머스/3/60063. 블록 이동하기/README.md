7eaa9bcc134f88b8e10ebb6573a99a8c48a38af7
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-06-14 16:15:28 +0900

    [level 3] Title: 블록 이동하기, Time: 67.16 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/README.md
+++ 프로그래머스/3/60063. 블록 이동하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 블록 이동하기 - 60063 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60063#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60063) 
 
 ### 성능 요약
 
-메모리: 40.1 MB, 시간: 32.67 ms
+메모리: 39.4 MB, 시간: 67.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 09일 21:50:56
+2025년 06월 14일 16:15:27
 
 ### 문제 설명
 
--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.js
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.js
@@ -1,173 +1,160 @@
-const dy = [-1, 1, 0, 0] // 상, 하 , 좌, 우
-const dx = [0, 0, -1, 1];
-
-class Pos {
-    constructor(ay, ax, by, bx, dir, dist) {
-        this.ay = ay;
-        this.ax = ax;
-        this.by = by;
-        this.bx = bx;
-        this.dir = dir;
-        this.dist = dist;
-    }
-}
-
 function solution(board) {
-    /*        
-        board
-        N x N 크기. 0 : 빈칸 1 : 벽
-        
-        visited
-        0 : 가로, 1 : 세로
-    */
-    
-    board = board.map((row) => row.map((item) => item === 0 ? true : false));
-
-    const SIZE = board.length;
-    const q = [new Pos(0, 0, 0, 1, 0, 0)];
-    const visited = Array.from({length : SIZE }, () => Array(SIZE).fill(0).map(() => [false, false]));
-    const answer = [];
-    
-    visited[0][0][0] = true;
-    
-    // bfs, visited
-
     /*
-        1. 현재 가로
-            1. 상 [ay - 1, ax] [by - 1, bx] 가 1
-            2. 하 [ay + 1, ax] [by + 1, bx] 가 1
-            3. 좌 [ay, ax - 1] 가 1
-            4. 우 [by, bx + 1] 가 1
-            5. [ay, ax] 축으로 아래 회전 : 2번 조건 
-            6. [ay, ax] 축으로 위 회전 : 1번
-            7. [by, bx] 축으로 아래 회전 : 2번
-            8. [by, bx] 축으로 위 회전 : 1번 
-            
-        2. 현재 세로
-            /*
-            1. 상 [ay - 1, ax] 가 1
-            2. 하 [by + 1, bx] 가 1
-            3. 좌 [ay, ax - 1] [by, bx - 1] 가 1
-            4. 우 [ay, ax + 1] [by, bx + 1] 가 1
-            5. [ay, ax] 축으로 왼쪽 회전 : 3번
-            6. [ay, ax] 축으로 오른쪽 회전 : 4번
-            7. [by, bx] 축으로 왼쪽 회전 : 3번
-            8. [by, bx] 축으로 오른쪽 회전 : 4번
-    */
-    while(q.length > 0) {
-        const cur = q.shift();
-        if((cur.ay === (SIZE - 1) && cur.ax === (SIZE - 1)) 
-           || (cur.by === (SIZE - 1) && cur.bx === (SIZE - 1))) return cur.dist;
+    - 로봇은 2 x 1 크기
+    - 지도는 N x N 크기
+    - 좌표는 (1, 1) ~ (N, N)
+    - 0 : 빈칸, 1 : 벽
+    - 로봇은 벽에 막히지 않는다면 회전이 가능함
+    
+    
+    목표
+    - 로봇이 차지하는 두 칸 중 어느 한 칸이라도 (N,N)에 도착하도록 하는 최소 시간을 구하라.
+    
+    제한 사항
+    - 초기 로봇은 (1,1) (1,2)에 놓여있고, 반드시 빈칸임
+    - 로봇은 반드시 목적지에 도착할 수 있음
+    - 5 <= N <= 100
+    
+    로봇이 이동 가능한 경우
+    
+    미방문 지역이어야 함(회전 포함)
+    회전 X
+    - 가로로 놓인 경우
+        - 오른쪽 이동 -> 우측 좌표 오른쪽 좌표가 0
+        - 왼쪽 이동 -> 좌측 좌표 왼쪽 좌표가 0
+        - 상단 이동 -> 상단 두 좌표가 0
+        - 하단 이동 -> 하단 두 좌표가 0
+    - 세로로 놓인 경우
+        - 오른쪽 이동 -> 우측 두 좌표가 0
+        - 왼쪽 이동 -> 좌측 두 좌표가 0
+        - 상단 이동 -> 상단 좌표 위쪽 좌표가 0
+        - 하단 이동 -> 하단 좌표 아래쪽 좌표가 0
+    
+    회전 O
+    - 가로로 놓인 경우
+        - 왼쪽을 축으로 아래로 회전 -> 하단 두 좌표가 0
+        - 왼쪽을 축으로 위로 회전 > 상단 두 좌표가 0
+        - 오른쪽을 축으로 아래로 회전 -> 하단 두 좌표가 0
+        - 오른쪽을 축으로 위로 회전 -> 상단 두 좌표가 0
+    - 세로로 놓인 경우
+        - 위쪽을 축으로 오른쪽 회전 -> 우측 두 좌표가 0
+        - 위쪽을 축으로 왼쪽 회전 -> 좌측 두 좌표가 0
+        - 아래쪽을 축으로 오른쪽 회전 -> 우측 두 좌표가 0
+        - 아래쪽을 축으로 왼쪽 회전 -> 좌측 두 좌표가 0
         
-        let nAy, nAx, nBy, nBx;
+    필요한 변수
+    - 현재 로봇 좌표 기억 (0, 0) ~ (N - 1, N - 1)
+    - 로봇이 어떤 방향으로 놓여있는가(가로, 세로)
+    - 방문 체크 배열(가로, 세로 방향)
+    
+    */   
+    const N = board.length;
+    const visited = new Array(N).fill().map(() => 
+                                            new Array(N).fill().map(() => 
+                                                                    new Array(2).fill(false)));
+    const dy = [-1, 1, 0, 0]; // 상, 하, 좌, 우
+    const dx = [0, 0, -1, 1];
+    
+    const q = [[0, 0, 0, 1, 0, 0]];
+    // [ay, ax, by, bx, dir, dist]
+    // dir 0 : 가로, 1 : 세로
+    
+    while(q.length > 0) {
+        const [ay, ax, by, bx, dir, dist] = q.shift();
+        
+        if(((ay === (N - 1) && ax === (N - 1)) || (by === (N - 1) && bx === (N - 1))))
+            return dist;
+        
+        if(visited[ay][ax][dir]) continue;
+        visited[ay][ax][dir] = true;
+        
+        let nay, nax, nby, nbx;
         let [canGoUp, canGoDown, canGoLeft, canGoRight] = [false, false, false, false];
         
-        if(cur.dir === 0) { // 가로
-            for(let i = 0; i < 4; i++) { // 상, 하, 좌, 우
-                nAy = cur.ay + dy[i];
-                nAx = cur.ax + dx[i];
-                nBy = cur.by + dy[i];
-                nBx = cur.bx + dx[i];
+        if(dir === 0) { // 현재 방향 가로
+            /*
+                회전 X
+                - 오른쪽 이동 -> 우측 좌표 오른쪽 좌표가 0
+                - 왼쪽 이동 -> 좌측 좌표 왼쪽 좌표가 0
+                - 상단 이동 -> 상단 두 좌표가 0
+                - 하단 이동 -> 하단 두 좌표가 0
+            */
+            for(let i = 0; i < 4; i++) {
+                [nay, nax, nby, nbx] = [ay + dy[i], ax + dx[i], by + dy[i], bx + dx[i]];
                 
-                if(!isValid(nAy, nAx, nBy, nBx)) continue;
-
-                    
-                if(i === 0) { // 상
-                    canGoUp = true
-                } else if(i === 1) { // 하
-                    canGoDown = true;
-                } 
+                if(!isValid(nay, nax, nby, nbx)) continue;
                 
-                if(visited[nAy][nAx][0]) continue;
-                q.push(new Pos(nAy, nAx, nBy, nBx, 0, cur.dist + 1));  
-                visited[nAy][nAx][0] = true;
-            }
-            
-            //  [ay, ax] 축으로 아래 회전 : 아래 이동 가능, 방문 여부
-            [nAy, nAx, nBy, nBx] = [cur.ay, cur.ax, cur.ay + 1, cur.ax];
-            if(canGoDown && !visited[nAy][nAx][1]) {
-                q.push(new Pos(nAy, nAx, nBy, nBx, 1, cur.dist + 1));  
-                visited[nAy][nAx][1] = true;
-            }
-            
-
-            //  [ay, ax] 축으로 위 회전 : 위 이동 가능, 방문 여부
-            [nAy, nAx, nBy, nBx] = [cur.ay - 1, cur.ax, cur.ay, cur.ax];
-            if(canGoUp && !visited[nAy][nAx][1]) {
-                q.push(new Pos(nAy, nAx, nBy, nBx, 1, cur.dist + 1));  
-                visited[nAy][nAx][1] = true;
-            }
-            
-            // [by, bx] 축으로 아래 회전 : 아래 이동 가능, 방문 여부
-            [nAy, nAx, nBy, nBx] = [cur.by, cur.bx, cur.by + 1, cur.bx];
-            if(canGoDown && !visited[nAy][nAx][1]){
-              q.push(new Pos(nAy, nAx, nBy, nBx, 1, cur.dist + 1));
-              visited[nAy][nAx][1] = true;
-            } 
-            
-            // [by, bx] 축으로 위 회전 : 위 이동 가능, 방문 여부
-            [nAy, nAx, nBy, nBx] = [cur.by - 1, cur.bx, cur.by, cur.bx];
-            if(canGoUp && !visited[nAy][nAx][1]) {
-                q.push(new Pos(nAy, nAx, nBy, nBx, 1, cur.dist + 1));
-                visited[nAy][nAx][1] = true;
-            }
-            
-        } else if(cur.dir === 1) { // 세로
-            for(let i = 0; i < 4; i++) { // 상, 하, 좌, 우
-                nAy = cur.ay + dy[i];
-                nAx = cur.ax + dx[i];
-                nBy = cur.by + dy[i];
-                nBx = cur.bx + dx[i];
- 
-                if(!isValid(nAy, nAx, nBy, nBx)) continue;
-
+                if(i === 0) canGoUp = true; // 상
+                else if(i === 1) canGoDown = true; // 하
                 
-                if(i === 2) { // 좌
-                    canGoLeft = true
-                } else if(i === 3) { // 우
-                    canGoRight = true;
-                } 
+                q.push([nay, nax, nby, nbx, 0, dist + 1]);
+            }
+            
+            // 회전 O
+            
+            if(canGoDown) {
+            // - 왼쪽을 축으로 아래로 회전 -> 하단 두 좌표가 0    
+                [nay, nax, nby, nbx] = [ay, ax, ay+1, ax];
+                if(isValid(nay, nax, nby, nbx)) q.push([nay, nax, nby, nbx, 1, dist+1]);
+            // - 오른쪽을 축으로 아래로 회전 -> 하단 두 좌표가 0
+                [nay, nax, nby, nbx] = [by, bx, by+1, bx];
+                if(isValid(nay, nax, nby, nbx)) q.push([nay, nax, nby, nbx, 1, dist+1]);
+            }
+            
+            if(canGoUp) {
+            // - 왼쪽을 축으로 위로 회전 > 상단 두 좌표가 0    
+                [nay, nax, nby, nbx] = [ay-1, ax, ay, ax];
+                if(isValid(nay, nax, nby, nbx)) q.push([nay, nax, nby, nbx, 1, dist+1]);
+            // - 오른쪽을 축으로 위로 회전 -> 상단 두 좌표가 0
+                [nay, nax, nby, nbx] = [by-1, bx, by, bx];
+                if(isValid(nay, nax, nby, nbx)) q.push([nay, nax, nby, nbx, 1, dist+1]);
+            }    
+        } else { // 현재 방향 세로
+            /*
+                회전 X
+                - 오른쪽 이동 -> 우측 두 좌표가 0
+                - 왼쪽 이동 -> 좌측 두 좌표가 0
+                - 상단 이동 -> 상단 좌표 위쪽 좌표가 0
+                - 하단 이동 -> 하단 좌표 아래쪽 좌표가 0    
+            */
+            for(let i = 0; i < 4; i++) {
+                [nay, nax, nby, nbx] = [ay + dy[i], ax + dx[i], by + dy[i], bx + dx[i]];
                 
-                if(visited[nAy][nAx][1]) continue;
-                q.push(new Pos(nAy, nAx, nBy, nBx, 1, cur.dist + 1));  
-                visited[nAy][nAx][1] = true;
+                if(!isValid(nay, nax, nby, nbx)) continue;
+                
+                if(i === 2) canGoLeft = true; // 좌
+                else if(i === 3) canGoRight = true; // 우
+                
+                q.push([nay, nax, nby, nbx, 1, dist + 1]);
             }
             
-            //  [ay, ax] 축으로 왼쪽 회전 : 왼쪽 이동 가능, 방문 여부
-            [nAy, nAx, nBy, nBx] = [cur.ay, cur.ax - 1, cur.ay, cur.ax];
-            if(canGoLeft && !visited[nAy][nAx][0]) {
-                q.push(new Pos(nAy, nAx, nBy, nBx, 0, cur.dist + 1));  
-                visited[nAy][nAx][0] = true;
+            // 회전 O
+            
+            if(canGoLeft) {
+                // - 위쪽을 축으로 왼쪽 회전 -> 좌측 두 좌표가 0
+                [nay, nax, nby, nbx] = [ay, ax-1, ay, ax];
+                if(isValid(nay, nax, nby, nbx)) q.push([nay, nax, nby, nbx, 0, dist+1]);
+                // - 아래쪽을 축으로 왼쪽 회전 -> 좌측 두 좌표가 0
+                [nay, nax, nby, nbx] = [by, bx-1, by, bx];
+                if(isValid(nay, nax, nby, nbx)) q.push([nay, nax, nby, nbx, 0, dist+1]);
             }
             
-            //  [ay, ax] 축으로 오른쪽 회전 : 오른쪽 이동 가능, 방문 여부
-            [nAy, nAx, nBy, nBx] = [cur.ay, cur.ax, cur.ay, cur.ax + 1];
-            if(canGoRight && !visited[nAy][nAx][0]) {
-                q.push(new Pos(nAy, nAx, nBy, nBx, 0, cur.dist + 1));  
-                visited[nAy][nAx][0] = true;
-            }
-            
-            // [by, bx] 축으로 왼쪽 회전 : 왼쪽 이동 가능, 방문 여부
-            [nAy, nAx, nBy, nBx] = [cur.by, cur.bx - 1, cur.by, cur.bx];
-            if(canGoLeft && !visited[nAy][nAx][0]) {
-                q.push(new Pos(nAy, nAx, nBy, nBx, 0, cur.dist + 1));  
-                visited[nAy][nAx][0] = true;
-            }
-            
-            //  [by, bx] 축으로 오른쪽 회전 : 오른쪽 이동 가능, 방문 여부
-            [nAy, nAx, nBy, nBx] = [cur.by, cur.bx, cur.by, cur.bx + 1];
-            if(canGoRight && !visited[nAy][nAx][0]) {
-                q.push(new Pos(nAy, nAx, nBy, nBx, 0, cur.dist + 1));  
-                visited[nAy][nAx][0] = true;
+            if(canGoRight) {
+                // - 위쪽을 축으로 오른쪽 회전 -> 우측 두 좌표가 0
+                [nay, nax, nby, nbx] = [ay, ax, ay, ax+1];
+                if(isValid(nay, nax, nby, nbx)) q.push([nay, nax, nby, nbx, 0, dist+1]);
+                // - 아래쪽을 축으로 오른쪽 회전 -> 우측 두 좌표가 0
+                [nay, nax, nby, nbx] = [by, bx, by, bx+1];
+                if(isValid(nay, nax, nby, nbx)) q.push([nay, nax, nby, nbx, 0, dist+1]);
             }
         }
     }
     
     function isValid(ay, ax, by, bx) {
-        return ((ay >= 0 && ax >= 0 && by >= 0 && bx >=0) 
-                && (ay < SIZE && ax < SIZE && by < SIZE && bx < SIZE)
-                && (board[ay][ax] && board[by][bx]));
+        if(!(ay >= 0 && by >= 0 && ax >= 0 && bx >= 0)) return false;
+        if(!(ay < N && by < N && ax < N && bx < N)) return false;
+        if(!(board[ay][ax] === 0 && board[by][bx] === 0)) return false;
+        
+        return true;
     }
-    // 6번 테스트만 틀림. 어떤걸 놓쳤나?
-    // canGo 변수들은 단순히 이동만 검사하는 것으로, 방문 여부를 검사하기 전에 체크함으로써 해결
 }
