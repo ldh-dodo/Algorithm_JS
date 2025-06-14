2fd4c69e009d52f014cff5aad43601c3e0b954f2
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-06-14 17:29:28 +0900

    [level 3] Title: 모두 0으로 만들기, Time: 696.88 ms, Memory: 229 MB -BaekjoonHub

--- 프로그래머스/3/76503. 모두 0으로 만들기/모두 0으로 만들기.js
+++ 프로그래머스/3/76503. 모두 0으로 만들기/모두 0으로 만들기.js
@@ -1,43 +1,53 @@
 function solution(a, edges) {
-    const SIZE = a.length;
-    let graph = Array.from({ length: SIZE }, () => []);
-    let visited = Array(SIZE).fill(false);
-    let res = BigInt(0);
-
-    const total = a.reduce((sum, val) => sum + val, 0);
-    if (total !== 0) return -1;
-
-    for (const [u, v] of edges) {
-        graph[u].push(v);
-        graph[v].push(u);
+    /*
+    각 점에 가중치가 부여된 트리
+    트리 모든 점들의 가중치를 0으로 만들어야 함
+    불가능하다면 -1을, 가능하다면 최소한의 행동으로 수행하고, 횟수를 반환
+    
+    풀이 전략
+    - 임의의 노드를 루트로 잡고, 해당 노드로 모든 요소를 모았을 때 0이 되어야 함
+    - 어떤 노드를 루트로 하던, 이 방식을 사용하면 항상 최소한의 행동을 기록할 수 있음
+    - 자식 -> 부모로 넘기는 절대 값이 행동 횟수이며, root를 가장 마지막으로 방문하기 위해 후위 순회 이용
+    
+    */
+    
+    const canSolve = a.reduce((acc, cur) => acc + cur, 0) === 0;
+    if(!canSolve) return -1;
+    
+    const st = [[0, -1]] // 자식노드, 부모노드
+    const paths = [];
+    const neighbor = new Array(a.length).fill().map(() => []);
+    const visited = new Array(a.length).fill(false);
+    let answer = BigInt(0);
+    
+    for(const [u, v] of edges) {
+        neighbor[u].push(v);
+        neighbor[v].push(u);
     }
-
-    let st = [[0, -1]]; // 현재, 부모 노드
-    let order = [];
     
     while(st.length > 0) {
-        const [cur, parent] = st.pop();
+        const [cur, par] = st.pop();
         
         if(visited[cur]) continue;
         
         visited[cur] = true;
-        order.push([cur, parent]);
+        paths.push([cur, par]);
         
-        for(const neighbor of graph[cur]) {
-            if(visited[neighbor]) continue;
-            
-            st.push([neighbor, cur]);
+        for(const next of neighbor[cur]) {
+            if(visited[next]) continue;
+            st.push([next, cur]);
         }
     }
     
-    while(order.length > 0) {
-        const [child, parent] = order.pop();
+    while(paths.length > 0) {
+        const [cur, par] = paths.pop();
+
+        if(par === -1) continue;
         
-        if(parent === -1) continue;
-        
-        a[parent] += a[child];
-        res += BigInt(Math.abs(a[child]));
+        a[par] += a[cur];
+        answer += BigInt(Math.abs(a[cur]));
     }
     
-    return res;
+    return answer;
+
 }
