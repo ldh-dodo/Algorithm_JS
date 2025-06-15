669820d046125ff7a31138465fa1fc152b38a645
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-06-15 15:09:50 +0900

    [level 3] Title: 최적의 행렬 곱셈, Time: 50.85 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.js
+++ 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.js
@@ -1,29 +1,33 @@
-function solution(matrixSizes) { 
-    function divideAndConquer(s, e) {
-        if(e - s === 1) return 0;
-        
-        let answer = Infinity;
-        
-        for(let m = s + 1; m < e; m++) {
-            let leftOpCnt = divideAndConquerMemo(s, m);
-            let rightOpCnt = divideAndConquerMemo(m, e);
-            let curOpCnt = matrixSizes[s][0] * matrixSizes[m][0] * matrixSizes[e - 1][1];
-            
-            answer = Math.min(answer, leftOpCnt + rightOpCnt + curOpCnt);
+function solution(matrixSizes) {
+    /*
+    [A, B] [B, C] 의 연산 횟수 : A x B x C
+    */
+    function divideAndConquerMemo(st, end) {
+        if(memo[st][end] === null) {
+            memo[st][end] = divideAndConquer(st, end);
         }
         
-        return answer;
-    }
-        
-    function divideAndConquerMemo(s, e) {
-        if(memo[s][e] === null) 
-            memo[s][e] = divideAndConquer(s, e);
-        
-        return memo[s][e];
+        return memo[st][end];
     }
     
-    const SIZE = matrixSizes.length;
-    let memo = Array.from({length : SIZE + 1}, () => Array.from({length : SIZE + 1}, () => null));
+    function divideAndConquer(st, end) {
+        if(end - st === 1) return 0;
+        
+        let minOpCnt = Infinity;
+        
+        for(let mid = st + 1; mid < end; mid++) {
+            const leftOpCnt = divideAndConquerMemo(st, mid);
+            const rightOpCnt = divideAndConquerMemo(mid, end);
+            const curOpCnt = matrixSizes[st][0] * matrixSizes[mid][0] * matrixSizes[end - 1][1];
+            
+            minOpCnt = Math.min(minOpCnt, (leftOpCnt + rightOpCnt + curOpCnt));
+        }
+        
+        return minOpCnt;
+    }
     
-    return divideAndConquer(0, SIZE);
+    const LEN = matrixSizes.length;
+    const memo = new Array(LEN + 1).fill().map(() => new Array(LEN + 1).fill(null));
+
+    return divideAndConquer(0, LEN);
 }
