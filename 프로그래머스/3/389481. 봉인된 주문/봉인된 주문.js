f8370ddffeb76df3f4880a221e5a307ad152c042
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-06-19 14:28:42 +0900

    [level 3] Title: 봉인된 주문, Time: 1127.24 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.js
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.js
@@ -1,35 +1,47 @@
 function solution(n, bans) {
     /*
-    - 각 주문은 알파벳 소문자 11글자 이하이다.
-    - 글자수 적은 주문 먼저 기록
-    - 글자수 같다면, 사전 순 기록
-    - 몇몇 주문 삭제되었고, 주문서에서 n번째 주문을 찾아야 함
+    주문은 알파벳 소문자로만 구성된다.
+    주문은 최대 11글자 이다.
+    주문은 정렬된 상태이다.
+    
+    글자수가 적은 주문부터 기록된다.
+    글자수가 같다면, 사전 순대로 기록된다.
+    
+    해당 주문서에서, 주문이 삭제된 후의 n번째 주문을 구하라.
+    
+    1 2 .. 26 27 ... 52 53 ... 676 
+    a b    z  aa     az ba     zz
+    
+    1 ~ 26^1 = 1자리
+    26^1 + 1 ~ 26^2 = 2자리
+    26^2 + 1 ~ 26^3 = 3자리
     */
-    // 26^1 ~ 26 ^ 11 
-    const BASE_26 = 26;
-    const alpha = "abcdefghijklmnopqrstuvwxyz";
-    let answer = "";
+    
+    let answer = '';
+    const BASE = 26;
+    const alpha = 'abcdefghijklmnopqrstuvwxyz';
     let len = 1;
-    const stringToIndex = (str) => [...str].reduce((acc, cur) => acc * BASE_26 + cur.charCodeAt(0) - 96, 0);
+    const stringToAsciiIdx = 
+          (str) => [...str].reduce((acc, cur) => BASE * acc + cur.charCodeAt() - 96, 0); 
     
-    while(n > 26 ** len) {
-        len++;
-    }
-
-   bans.sort((a,b) => {
-       if(a.length === b.length) return a.localeCompare(b);
-       return a.length - b.length;
-   });
+    while(n > BASE ** len) len++;
     
+    bans.sort((a, b) => {
+        if(a.length === b.length) return a.localeCompare(b);
+        return a.length - b.length;
+    });
+    
+    bans = bans.map((ban) => ban.split(''));
+    bans = bans.map(stringToAsciiIdx);
     
     for(const ban of bans) {
-        if(ban.length > len) break;
-        if(n >= stringToIndex(ban)) n++;
+        if(n < ban) break;
+        n++;
     }
     
     while(len > 0) {
-        answer = alpha[(n - 1) % BASE_26] + answer;
-        n = Math.floor((n - 1) / BASE_26);
+        answer = alpha[(n - 1) % BASE] + answer;
+        n = Math.floor((n - 1) / BASE);
         len--;
     }
     
