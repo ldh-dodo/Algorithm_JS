4d5d25b26dfee5f8201455b3fc68c84cc556089a
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2025-07-07 17:43:12 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 0.70 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.js
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.js
@@ -1,122 +1,121 @@
 function solution(beginning, target) {
-    const rowLen = beginning.length;
-    const colLen = beginning[0].length;
-    let [res1, res2, res3, res4] = [0, 0, 0, 0];
-    let flagArr = Array.from({length : rowLen}, () => Array.from({length : colLen}, () => 0));
-    // beginning과 target이 서로 다른 요소는 1, 같은 요소는 0으로.
-    for(let i = 0; i < rowLen; i++) {
-        for(let j = 0; j < colLen; j++) {
-            if(beginning[i][j] !== target[i][j]) {
-                flagArr[i][j] = 1;
-            }
-        }
-    }
+    /*
+        초기 상태에서 목표상태가 되기 위한 동전 뒤집기 횟수의 최솟값
+        목표 상태를 만들지 못하면 -1
+        
+        행, 열을 뒤집는 순서는 최종 결과에 영향을 주지 않음
+        
+        요소의 첫번째를 기준으로, 4가지 경우가 존재 
+        
+        '같다' 라는 말은 target의 첫번째 요소와 같다는 의미
+        
+        행 첫번째 요소가 같을 때 뒤집고, 뒤집은 후에 열의 첫번째 요소가 같을 때 뒤집기
+        행 첫번째 요소가 같을 때 뒤집고, 뒤집은 후에 열의 첫번째 요소가 다를 때 뒤집기
+        행 첫번째 요소가 다를 때 뒤집고, 뒤집은 후에 열의 첫번째 요소가 같을 때 뒤집기
+        행 첫번째 요소가 다를 때 뒤집고, 뒤집은 후에 열의 첫번째 요소가 다를 때 뒤집기
+        
+        배열을 target과 다른 부분만 1로, 같은 부분은 0으로 표시
+    */
+    
 
-    // 행 다른 것 -> 열 다른 것
-    let copyFlag = JSON.parse(JSON.stringify(flagArr));
-
-    for(let i = 0; i < rowLen; i++) {
-        if(copyFlag[i][0] === 0) continue;
-
-        res1++;
-
-        for(let j = 0; j < colLen; j++) {
-            copyFlag[i][j] = 1 ^ copyFlag[i][j];
+    const rowSize = target.length;
+    const columnSize = target[0].length;
+    const flagArr = new Array(rowSize).fill().map(() => new Array(columnSize).fill(0));
+    
+    for(let i = 0; i < rowSize; i++) {
+        for(let j = 0; j < columnSize; j++) {
+            if(beginning[i][j] !== target[i][j]) flagArr[i][j] = 1;
         }
     }
     
-    for(let j = 0; j < colLen; j++) {
-        if(copyFlag[0][j] === 0) continue;
-
-        res1++;
-
-        for(let i = 0; i < rowLen; i++) {
-            copyFlag[i][j] = 1 ^ copyFlag[i][j];
-        }
-    }
-
-    let isSolved1 = copyFlag.every((row) => row.every((flag) => flag === 0));
-
-    // 열 다른 것 -> 행 다른 것
-    copyFlag = JSON.parse(JSON.stringify(flagArr));
-
-    for(let j = 0; j < colLen; j++) {
-        if(copyFlag[0][j] === 0) continue;
-
-        res2++;
-
-        for(let i = 0; i < rowLen; i++) {
-            copyFlag[i][j] = 1 ^ copyFlag[i][j];
-        }
-    }
-    for(let i = 0; i < rowLen; i++) {
-        if(copyFlag[i][0] === 0) continue;
-
-        res2++;
-
-        for(let j = 0; j < colLen; j++) {
-            copyFlag[i][j] = 1 ^ copyFlag[i][j];
-        }
-    }
-
-    const isSolved2 = copyFlag.every((row) => row.every((flag) => flag === 0));
-
-    // 행 안다른 것 -> 열 다른 것
-    copyFlag = JSON.parse(JSON.stringify(flagArr));
-
-    for(let i = 0; i < rowLen; i++) {
-        if(copyFlag[i][0] === 1) continue;
-
-        res3++;
-
-        for(let j = 0; j < colLen; j++) {
-            copyFlag[i][j] = 1 ^ copyFlag[i][j];
-        }
-    }
-
-    for(let j = 0; j < colLen; j++) {
-        if(copyFlag[0][j] === 0) continue;
-
-        res3++;
-
-        for(let i = 0; i < rowLen; i++) {
-            copyFlag[i][j] = 1 ^ copyFlag[i][j];
-        }
-    }
-
-    const isSolved3 = copyFlag.every((row) => row.every((flag) => flag === 0));
+    let [min, tempCnt] = [Infinity, 0];
+    const isSolved = (copyFlag) => copyFlag.every((row) => row.every((flag) => flag === 0));
+    // 행 첫번째 요소가 같을 때 뒤집고, 뒤집은 후에 열의 첫번째 요소가 같을 때 뒤집기
+    let copyArr = JSON.parse(JSON.stringify(flagArr));
     
-    // 열 안다른 것 -> 행 다른 것
-    copyFlag = JSON.parse(JSON.stringify(flagArr));
-
-    for(let j = 0; j < colLen; j++) {
-        if(copyFlag[0][j] === 1) continue;
-
-        res4++;
-
-        for(let i = 0; i < rowLen; i++) {
-            copyFlag[i][j] = 1 ^ copyFlag[i][j];
+    for(let i = 0; i < rowSize; i++) {
+        if(copyArr[i][0] === 1) continue;
+        
+        tempCnt++;
+        for(let j = 0; j < columnSize; j++) {
+            copyArr[i][j] = 1 ^ copyArr[i][j];
         }
     }
-
-    for(let i = 0; i < rowLen; i++) {
-        if(copyFlag[i][0] === 0) continue;
-
-        res4++;
-
-        for(let j = 0; j < colLen; j++) {
-            copyFlag[i][j] = 1 ^ copyFlag[i][j];
+    
+    for(let j = 0; j < columnSize; j++) {
+        if(copyArr[0][j] === 1) continue;
+        
+        tempCnt++;
+        for(let i = 0; i < rowSize; i++) {
+            copyArr[i][j] = 1 ^ copyArr[i][j];
         }
     }
-
-    const isSolved4 = copyFlag.every((row) => row.every((flag) => flag === 0));
+    if(isSolved(copyArr)) min = Math.min(min, tempCnt);
     
-    if(!isSolved1) res1 = Infinity;
-    if(!isSolved2) res2 = Infinity;
-    if(!isSolved3) res3 = Infinity;
-    if(!isSolved4) res4 = Infinity;
+    // 행 첫번째 요소가 같을 때 뒤집고, 뒤집은 후에 열의 첫번째 요소가 다를 때 뒤집기
+    tempCnt = 0;
+    copyArr = JSON.parse(JSON.stringify(flagArr));
+    for(let i = 0; i < rowSize; i++) {
+        if(copyArr[i][0] === 1) continue;
+        
+        tempCnt++;
+        for(let j = 0; j < columnSize; j++) {
+            copyArr[i][j] = 1 ^ copyArr[i][j];
+        }
+    }
     
-    const min = Math.min(res1, res2, res3, res4);
+    for(let j = 0; j < columnSize; j++) {
+        if(copyArr[0][j] === 0) continue;
+        
+        tempCnt++;
+        for(let i = 0; i < rowSize; i++) {
+            copyArr[i][j] = 1 ^ copyArr[i][j];
+        }
+    }
+    if(isSolved(copyArr)) min = Math.min(min, tempCnt);
+    
+    // 행 첫번째 요소가 다를 때 뒤집고, 뒤집은 후에 열의 첫번째 요소가 같을 때 뒤집기
+    tempCnt = 0;
+    copyArr = JSON.parse(JSON.stringify(flagArr));
+    for(let i = 0; i < rowSize; i++) {
+        if(copyArr[i][0] === 0) continue;
+        
+        tempCnt++;
+        for(let j = 0; j < columnSize; j++) {
+            copyArr[i][j] = 1 ^ copyArr[i][j];
+        }
+    }
+    
+    for(let j = 0; j < columnSize; j++) {
+        if(copyArr[0][j] === 1) continue;
+        
+        tempCnt++;
+        for(let i = 0; i < rowSize; i++) {
+            copyArr[i][j] = 1 ^ copyArr[i][j];
+        }
+    }
+    if(isSolved(copyArr)) min = Math.min(min, tempCnt);
+    // 행 첫번째 요소가 다를 때 뒤집고, 뒤집은 후에 열의 첫번째 요소가 다를 때 뒤집기
+    tempCnt = 0;
+    copyArr = JSON.parse(JSON.stringify(flagArr));
+    for(let i = 0; i < rowSize; i++) {
+        if(copyArr[i][0] === 0) continue;
+        
+        tempCnt++;
+        for(let j = 0; j < columnSize; j++) {
+            copyArr[i][j] = 1 ^ copyArr[i][j];
+        }
+    }
+    
+    for(let j = 0; j < columnSize; j++) {
+        if(copyArr[0][j] === 0) continue;
+        
+        tempCnt++;
+        for(let i = 0; i < rowSize; i++) {
+            copyArr[i][j] = 1 ^ copyArr[i][j];
+        }
+    }
+    if(isSolved(copyArr)) min = Math.min(min, tempCnt);
     
     return min === Infinity ? -1 : min;
 }
