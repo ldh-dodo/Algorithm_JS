ba02901f737f0ce7f6f849f5e876a34b4165e65f
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-13 14:19:55 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.80 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let dp = Array(n + 1).fill(0);
+    
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    if(n === 1) return dp[1];
+    if(n === 2) return dp[2];
+    
+    for(let i = 3; i <= n; i++) {
+        dp[i] = ((dp[i - 2] + dp[i - 1]) % 1000000007);
+    }
+    
+    return dp[n];
+}
