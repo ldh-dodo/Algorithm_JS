8a80f7fa3b7243489257560b13d99d24ad98e0d7
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-17 13:04:56 +0900

    [level 3] Title: 숫자 게임, Time: 87.45 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
@@ -1,23 +1,32 @@
 function solution(A, B) {
-    // 숫자 같으면 승점 얻지 X
-    // B팀은 A팀의 경기에서 이길 수 있다면, 가능한 작은 수로 이기는 것이 좋음
+    /*
+    A B 팀
+    N N 
     
-    const SIZE = A.length;
-    let answer = 0;
-    let bIdx = 0;
+    모든 사원 : 무작위 자연수, 한 번씩 경기
+    각 팀당 한 명씩 겨루고, 숫자 큰 쪽 승리
+    이긴 팀 승점 1점. 숫자 같다면 승점 X
+    A팀 순서 공개 -> B팀이 승점 높이는 방법으로 출전 순서 정해야 함. 이 때 B팀이 얻는 승점
     
+    전략: 가장 적은 B로 A를 이겨라
+
+    */
+    const N = A.length;
     A.sort((a, b) => a - b);
     B.sort((a, b) => a - b);
-
-    for(let i = 0; i < SIZE; i++) { // A팀
-        while(bIdx < SIZE) {
-            if(A[i] < B[bIdx]) {
-                answer++;
+    
+    let bIdx = 0;
+    let answer = 0;
+    
+    for(let i = 0; i < N; i++) {
+        const curA = A[i];
+        while(bIdx < N) {
+            if(curA < B[bIdx]) {
                 bIdx++;
+                answer++;
                 break;
             } else bIdx++;
         }
     }
-    
     return answer;
 }
