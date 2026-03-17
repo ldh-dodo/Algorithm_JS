15492529a12f52c72eb5584f9b3cac5be6353422
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-17 14:12:03 +0900

    [level 3] Title: 기지국 설치, Time: 3.18 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
@@ -1,24 +1,42 @@
 function solution(n, stations, w) {
+    /*
+    기지국을 최소로 설치하면서, 모든 아파트에 전파를 전달해야 함
+    범위 : [station - W, station + W]
+    
+    전략
+    - 각 기지국의 왼쪽 범위를 계산하고, 마지막 기지국은 오른쪽까지 추가로 계산
+    
+    기지국 범위 : 2 * W + 1
+    
+    (1, 6) -> 6 - 1 + 1 -> Math.floor(6 / range) = 2
+    
+    9 - W -> 9 - 2 = 7
+    9 + W -> 9 + 2 = 11
+    */
     let answer = 0;
     let left = 1;
+    const range = 2 * w + 1;
+    
+    // (left, station - W - 1)
+    for(const station of stations) {
+        if(station === 1) {
+            left = station + w + 1;
+            continue;
+        }
+        
+        const right = station - w - 1;
+        const dist = right - left + 1;
 
-    for(let i = 0; i < stations.length; i++) {
-        if(left > n) break;
-        // 각 stations의 왼쪽개수만 처리
-        // left ~ cur - W - 1 인덱스까지 처리.
-        // 이후 left <- cur + W + 1
         
-        const cur = stations[i];
-        const right = cur - w - 1;
-        const cnt = right - left + 1;
-        
-        answer += Math.ceil(cnt / (2 * w + 1));
-        left = cur + w + 1;
+        answer += Math.ceil(dist / range);
+        left = station + w + 1;
     }
     
-    // 마지막 stations의 오른쪽 처리
-    if(left <= n) {
-        answer += Math.ceil((n - left + 1) / (2 * w + 1));
+    if(left <= n) { // 남은 오른쪽 처리 (left, n);
+        const right = n;
+        const dist = right - left + 1;
+
+        answer += Math.ceil(dist / range);
     }
     
     return answer;
