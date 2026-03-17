6e2347fb5a5aab97dd83a9fbe3b89afd45ac42b6
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-17 13:26:11 +0900

    [level 3] Title: 단속카메라, Time: 26.60 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,25 +1,30 @@
 function solution(routes) {
-    let answer = 0;
-    let checked = new Set();
+    /*
+    모든 차량이 한 번은 단속용 카메라를 만나도록 하는 최소 단속용 카메라 대수
+    route : [진입 시점, 나간 시점]
     
-    // return : 모든 차량이 단속용 카메라를 한 번은 만나도록 하는 최소 카메라 수
-    // 1 <= routes.length <= 10,000
-    // routes = [진입 지점, 진출 지점]
-    // -30,000 <= 진입, 진출 지점 <= 30,000
+    전략
+    - 나간시점이 작은순으로 정렬
+    - prevEnd < nextStart 면 겹치지 않는 범위, prevEnd
     
-    routes.sort((a,b) => a[1] - b[1]);
+     -19 -16
+    -20  -16
+   -21   -16
+             -14 -13
+    */
+    routes.sort((a, b) => a[1] - b[1]);
+    let answer = 1;
+    let [prevSt, prevEnd] = [routes[0][0], routes[0][1]];
 
-    let be = -30001;
-    
-    for(let i = 0; i < routes.length; i++) {
-        const [s, e] = routes[i];
+    for(let i = 1; i < routes.length; i++) {
+        const [curSt, curEnd] = routes[i];
         
-        if(s > be) {
+        if(prevEnd < curSt) {
+            console.log(prevSt,prevEnd, curSt,curEnd);
             answer++;
-            be = e;
+            [prevSt, prevEnd] = [curSt, curEnd];
         }
     }
-    
     
     return answer;
 }
