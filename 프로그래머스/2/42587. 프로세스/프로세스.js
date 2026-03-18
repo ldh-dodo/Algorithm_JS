377ef7af27b8e5557a2246f02bbcc8fc9f8078e1
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-18 15:20:27 +0900

    [level 2] Title: 프로세스, Time: 10.15 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,14 +1,19 @@
 function solution(q, location) {
     let head = 0;
-    let answer = 0;
+    let answer = 0
+    
+    // 최적화 방법
+    // sorted를 통해서, 현재의 최고 target을 구하자.
+    // 현재는 slcie, map때문에 O(N^2)이 되어 비효율적.
+    
+    const sorted = [...q].sort((a, b) => b - a);
     
     q = q.map((num, idx) => [num, idx]); // [priority, idx];
     while(head < q.length) {
-        const max = Math.max(...q.slice(head).map(([prty, idx]) => prty))
-
         const [prty, idx] = q[head++];
-
-        if(prty !== max) {
+        
+        console.log(prty, idx);
+        if(prty !== sorted[answer]) {
             q.push([prty, idx]);
             continue;
         }
