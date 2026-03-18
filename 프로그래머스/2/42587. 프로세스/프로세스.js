4ac9f2e41b04397a9d245e6965809e9074fbe745
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-18 15:12:45 +0900

    [level 2] Title: 프로세스, Time: 24.70 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,23 +1,19 @@
-function solution(prty, idx) {
+function solution(q, location) {
+    let head = 0;
     let answer = 0;
     
-    while(prty.length !== 0){
-        let firstItem = prty[0];
-        let max = Math.max(...prty);
-        
-        if(max !== firstItem){ // 첫번째 요소가 최댓값이 아닐 때
-            idx = (idx - 1 >= 0) ? idx-1 : prty.length -1;
-            prty.shift(); // 첫번째 요소 제거
-            prty.push(firstItem); // 맨 뒤에 추가
-        } else { // 첫번째 요소가 최댓값일 때
-            answer++;
-            prty.shift();
-            if(idx === 0){//첫번째 요소가 찾고자 하는 위치일 때
-                break;
-            } 
-            --idx;
+    q = q.map((num, idx) => [num, idx]); // [priority, idx];
+    while(head < q.length) {
+        const max = Math.max(...q.slice(head).map(([prty, idx]) => prty))
+
+        const [prty, idx] = q[head++];
+
+        if(prty !== max) {
+            q.push([prty, idx]);
+            continue;
         }
+        
+        answer++;
+        if(idx === location) return answer;
     }
-  
-    return answer;
 }
