4b639081d1e3665e143274c0ffd741650f9cf3f1
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-19 15:29:34 +0900

    [level 3] Title: 스티커 모으기(2), Time: 9.72 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -1,27 +1,38 @@
 function solution(sticker) {
-    const SIZE = sticker.length;
-          
-    if(SIZE === 1) return sticker[0];
+    // 처음을 뜯는 경우
+    // [0] 뜯고 시작. [1], [length - 1] 은 제외
     
-    let dp1 = new Array(SIZE).fill(0);
-    let dp2 = new Array(SIZE).fill(0);
+    // 처음을 안뜯는 경우
+    // [0]은 제외
+    const len = sticker.length;
+    
+    if(len === 1) {
+        return sticker[0];
+    } else if(len === 2) {
+        return Math.max(sticker[0], sticker[1]);
+    }
+    
+    let max = -Infinity;
+    const dp1 = Array(len).fill(-Infinity);
+    const dp2 = Array(len).fill(-Infinity);
     
     dp1[0] = sticker[0];
-    dp1[1] = sticker[0];
+    dp1[1] = dp1[0];
     
-    for(let i = 2; i < SIZE - 1; i++) {
-        dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i]);
+    for(let i = 2; i < len - 1; i++) {
+        dp1[i] = Math.max(dp1[i - 2] + sticker[i], dp1[i - 1]);
+        if(dp1[i] > max) max = dp1[i];
     }
-    
-    let max = dp1[SIZE - 2];
-
+   
+    dp2[0] = 0;
     dp2[1] = sticker[1];
-    
-    for(let i = 2; i < SIZE; i++) {
-        dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i]);
+        
+    for(let i = 2; i < len; i++) {
+        dp2[i] = Math.max(dp2[i - 2] + sticker[i], dp2[i - 1]);
+        if(dp2[i] > max) max = dp2[i];
     }
+
     
-    max = Math.max(max, dp2[SIZE - 1]);
     
     return max;
 }
