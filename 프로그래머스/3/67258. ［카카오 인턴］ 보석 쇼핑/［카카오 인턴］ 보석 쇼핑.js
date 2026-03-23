c7f9382f0f5d6b4bad9f186dae0b28e10b0b966f
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-23 16:57:08 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 31.70 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,39 +1,40 @@
 function solution(gems) {
-    // sliding window
-    let answer = [];
+    /*
+    모든 종류의 보석을 적어도 1개 이상 포함하는 가장 짧은 구간
+    -> 구간이 여러개면 시작 진열대 번호가 가장 작은 구간
     
-    let totalType = new Set(gems).size;
-    let map = new Map();
+    return [시작 진열대 번호, 끝 진열대 번호]
+    */
+    const gemSet = new Set(gems);
+    const gemMap = new Map();
     
-    let left = 0;
-    let right = 0;
-    let minRange = Infinity;
+    let left = 0, right = 0;
+    let answer = [0, gems.length];
     
-    while(right < gems.length) {
-        const gem = gems[right++];
-        const qty = map.get(gem) || 0;
+    gemMap.set(gems[0], 1);
+    
+    while(left < gems.length && right < gems.length) {
+        const isAll = gemMap.size === gemSet.size;
         
-        map.set(gem, qty + 1);
-        
-        while(map.size === totalType) {
-            const range = right - left;
-            
-            if(range < minRange) {
-                minRange = range;
-                answer = [left + 1, right];
-            }
-            
-            let leftGem = gems[left];
-            let leftQty = map.get(leftGem);
-            
-            if(leftQty > 1) {
-                map.set(leftGem, leftQty - 1);
-            } else {
-                map.delete(leftGem);
-            }
+        if(isAll) {
+            if(answer[1] - answer[0] > right - left) answer = [left + 1, right + 1];
             
             left++;
+            const prevGem = gems[left - 1];
+            const prevCnt = gemMap.get(prevGem);
+            
+            if(prevCnt - 1 === 0) gemMap.delete(prevGem);
+            else gemMap.set(prevGem, prevCnt - 1);
+        } else {
+            right++;
+            
+            if(right >= gems.length) break;
+            const nextGem = gems[right];
+            const nextCnt = gemMap.get(nextGem) || 0;
+            
+            gemMap.set(nextGem, nextCnt + 1);
         }
     }
+    
     return answer;
 }
