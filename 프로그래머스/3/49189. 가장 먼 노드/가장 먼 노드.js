87fe4d54208f54324392d7d7ea010304471fe5c4
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-24 14:32:33 +0900

    [level 3] Title: 가장 먼 노드, Time: 50.12 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,38 +1,40 @@
 function solution(n, edge) {
-    // node : 1-based
-    // 1번 노드에서 최단 경로 기준가장 멀리 떨어진 노드의 갯수
-        
-    let answer = 0;
-    let graph = new Array(n + 1).fill().map(() => []);
-    let visited = new Array(n + 1).fill(false);
-    let q = [];
+    /*
+    노드: 1 ~ n 
+    
+    return: 1번 노드에서 가장 멀리 떨어진 노드의 갯수
+    가장 멀리 떨어진 노드: 최단 경로로 이동했을 때, 간선의 개수가 가장 많은 노드
+    
+    각 노드마다 가중치 1이라 BFS로 가능하다고 판단
+    */
+    const graph = Array.from({length : n + 1}, () => []);
+    const dist = Array(n + 1).fill(null);
+    const visited = Array(n + 1).fill(false);
     
     for(const [u, v] of edge) {
         graph[u].push(v);
         graph[v].push(u);
     }
     
-    q = [[1, 0]]; // [node, dist];
+    const q = [1];
     visited[1] = true;
-    let maxDist = -Infinity;
+    dist[1] = 0;
     
-    while(q.length > 0) {
-        const [node, dist] = q.shift();
-
-        if(dist > maxDist) {
-            maxDist = dist;
-            answer = 1;
-        } else {
-            answer++;
-        }
+    let head = 0;
+    
+    while(head < q.length) {
+        const cur = q[head++];
         
-        for(const neighbor of graph[node]) {
-            if(visited[neighbor]) continue;
+        for(const next of graph[cur]) {
+            if(visited[next]) continue;
             
-            visited[neighbor] = true;
-            q.push([neighbor, dist + 1]);
-        }
+            q.push(next);
+            dist[next] = dist[cur] + 1;
+            visited[next] = true;
+        } 
     }
     
-    return answer;
+    const max = Math.max(...dist);
+    
+    return dist.filter((d) => d === max).length;
 }
