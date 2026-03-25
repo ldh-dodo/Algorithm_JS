3f8f248aa80fda79be365d6eddeb56fa08b955bf
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-25 14:14:27 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 9.90 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
@@ -1,44 +1,18 @@
 function solution(sequence) {
-    let answer = 0;
-    
-    /*
-    
-    펄스 수열 [1, -1, 1, -1] or [-1, 1, -1, 1]
-    연속 부분 수열 x 펄스 수열 = 연속 펄스 부분 수열
-
-    합을 누적하면서, 합이 음수가 되는 시점은 최대가 될 수 없는 경우이므로, 0으로 초기화.
-    합이 양수인 시점에는 최댓값 갱신
-    */
-    const LEN = sequence.length;
-    let [se1, se2] = [[...sequence], [...sequence]];
-    
-    se1 = se1.map((item, idx) => idx % 2 === 1 ? item * -1 : item);
-    se2 = se2.map((item, idx) => idx % 2 === 0 ? item * -1 : item);
-    
-    let max = -Infinity;
-    let sum = 0;
-    
-    for(const num of se1) {
-        if(sum + num < 0) {
-            sum = 0;
-            continue;
-        }
+    let max1 = -Infinity, max2 = -Infinity;
+    let sum1 = 0, sum2 =0;
+   
+    for(let i = 0; i < sequence.length; i++) {
+        const num = sequence[i];
         
-        sum += num;
-        max = Math.max(max, sum);
+        const num1 = num * (i % 2 === 0 ? 1 : -1);
+        sum1 = Math.max(num1, sum1 + num1);
+        max1 = Math.max(sum1, max1);
+        
+        const num2 = num * (i % 2 === 0 ? -1 : 1);
+        sum2 = Math.max(num2, sum2 + num2);
+        max2 = Math.max(sum2, max2);
     }
     
-    sum = 0;
-    
-    for(const num of se2) {
-        if(sum + num < 0) {
-            sum = 0;
-            continue;
-        }
-        
-        sum += num;
-        max = Math.max(max, sum);
-    }
-    
-    return max;
+    return Math.max(max1, max2);
 }
