ff8410306025cad1d6e26e3556d9831f3684c65d
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-28 13:59:02 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.02 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,57 +1,44 @@
 function solution(jobs) {
     /*
-        작업 번호, 요청 시각, 소요 시간
-        
-        우선순위
-        1. 소요 시간 짧은 것
-        2. 요청 시각 빠른 것
-        3. 작업 번호 작은 것
-        
-        반환 시간 = 종료 시각 - 요청 시각
-        반환 시간의 평균 구하기.        
+    대기 큐 : [요청 시각, 소요 시간, 작업 번호]
+    우선순위 : 소요시간 짧은 순 -> 요청 시각 빠른 순 -> 작업 번호 작은 순
+    
+    반환 시간 : 작업 종료 시각 - 요청 시각
+    return : 반환 시간 평균의 정수 부분
     */
-    // 1. [작업 번호, 요청 시각, 소요 시간] 으로 배열 만들기
-    const len = jobs.length;
-    jobs = jobs.map((el, idx) => [idx, ...el]);
+    
+    jobs = jobs.map(([reqT, takeT], idx) => [reqT, takeT, idx]).sort((a, b) => a[1] - b[1] || a[0] - b[0] || a[2] - b[2]);
+    
+    const N = jobs.length;
+    let fin = 0;
+    let t = 0;
+    let idx = 0;
+    let minReqT = -1;
+    let sum = 0;
+    
+    while(fin < N) {
+        if(idx >= jobs.length) {
+            minReqT = Math.min(...jobs.map((job) => job[0]));
+            t = minReqT;
+            idx = 0;
+        }
+        
+        const [reqT, takeT] = jobs[idx];
+        
+        if(t < reqT) {
+            idx++;
+            continue;
+        }
+        
 
-    // 2. 우선순위로 정렬
-    jobs.sort((a, b) => {
-        if(a[2] === b[2] && a[1] === b[1]) return a[0] - b[0]
-        if(a[2] === b[2]) return a[1] - b[1];
-        return a[2] - b[2];
-    });
-    
-    let time = 0;
-    let turnaroundTimes = [];
-    
-    // 3. 반복문으로 각 작업의 종료 시각을 구하기
-    let answer = 0;
-    let finishCnt = 0;
-    let i = 0;
-    let currentTime = 0;
-    
-    while(finishCnt < len) {
-        if(i >= jobs.length) {
-            const minRequestTime = Math.min(...jobs.map((el) => el[1]));
-            currentTime = minRequestTime;
-            i = 0;
-        }
-        
-        let [_, st, cost] = jobs[i];
-        
-        if(currentTime < st) i++;
-        else {
-            finishCnt++;
-            currentTime += cost;
-            answer += (currentTime - st);
-            
-            jobs.splice(i, 1);
-            i = 0;
-        }
+        t += takeT;
+        fin++;
+        sum += t - reqT;
+        jobs.splice(idx, 1);
+        idx = 0;
     }
-       
-    // 4. 평균 구하기
-    answer = Math.floor(answer / len);
     
-    return answer;
+    const avg = Math.floor(sum / N);
+    
+    return avg;
 }
