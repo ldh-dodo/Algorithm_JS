37a942eafa1db381f408c3cd70e0f4e9f701c491
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-03-31 14:12:12 +0900

    [level 3] Title: 순위, Time: 11.70 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,37 +1,41 @@
 function solution(n, results) {
     /*
-    1 ~ n 번 권투 선수
-    권투는 1 대 1
-    실력이 좋다면 반드시 이김
+    1 ~ n 번 권투선수
+    A 선수 실력 > B 선수 -> A 선수 승리
     
-    경기 결과가 분실되었고, 정확하게 순위를 매길 수 있는 선수의 수를 반환
-    
-    특정 노드에서 출발해서 목표 노드까지 도달할 수 있는 경우가 n - 1 이라면, 결과를 도출할 수 있음
+    return : 정확하게 순위를 매길 수 있는 선수의 수
+          1  2  3  4  5
+    [2]   X  me  X  X  O -> 4
+    [5]      X
     */
     
-    let floyd = new Array(n + 1).fill().map(() => new Array(n + 1).fill(false));
-    let checked = new Array(n + 1).fill(0);
+    const graph = Array.from({length : n + 1}, () => Array(n + 1).fill(false));
+    const rel = Array(n + 1).fill(0);
     
     for(const [u, v] of results) {
-        floyd[u][v] = true;
+        graph[u][v] = true;
     }
-
+    
     for(let k = 1; k <= n; k++) {
+        // i -> k -> j가 존재하는지
         for(let i = 1; i <= n; i++) {
             for(let j = 1; j <= n; j++) {
-                if(floyd[i][k] && floyd[k][j]) floyd[i][j] = true;
+                if(graph[i][k] && graph[k][j]) {
+                    graph[i][j] = true;
+                }
             }
         }
     }
-    
-   for(let i = 1; i <= n; i++) {
-       for(let j = 1; j <= n; j++) {
-           if(floyd[i][j]) {
-               checked[i] += 1;
-               checked[j] += 1;
-           }
+
+    for(let i = 1; i <= n; i++) {
+        for(let j = 1; j <= n; j++) {
+            if(i === j) continue;
+            if(!graph[i][j]) continue;
+            
+            rel[i]++;
+            rel[j]++;
         }
-   }
+    }
     
-    return checked.reduce((acc, cur) => (cur === n - 1 ? acc + 1 : acc), 0);
+    return rel.filter((cnt) => cnt >= (n - 1)).length;
 }
