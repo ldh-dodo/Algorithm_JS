6fd533080fd847435c2a5b36145f2617902ce81c
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-04-27 12:53:35 +0900

    [level 3] Title: 광고 삽입, Time: 987.23 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.js
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.js
@@ -1,77 +1,57 @@
-function solution(playTime, advTime, logs) {
+function solution(play_time, adv_time, logs) {
     /*
-    시청자들이 가장 많이 보는 구간에 공익광고를 삽입
-    광고는 원래 영상과 동시에 재생되는 형태로 제공
+    시청자들이 가장 많이 보는 구간에 공익 광고
+    광고는 원래 영상과 동시에 재생됨(PIP)
     
-    playTime : 동영상 재생 시간(HH:MM:SS)
-    advTime : 공익광고 재생시간(HH:MM:SS)
-    
-    logs : 시청자들이 동영상의 어떤 구간을 재생했는지에 대한 재생구간 기록
-    (H1:M1:S1-H2:M2:S2)
-    
-    누적 재생 시간이 가장 많기 위해서는 광고를 삽입했을 때 총 시청 시간이 최대가 되어야함
-    
-    해결 전략
-    1. 모든 시간을 초로 변환한다.
-    2. playTime 크기만큼의 배열을 만들고, 시청자 수 누적합 계산
-        누적합을 계산할 때, 시작 지점을 1로, 종료지점보다 1초 클 때 -1로 설정하고, 한 번에 계산한다.
-        
-    3. 시청자 수가 가장 많았던 시작 지점을 구한다.
-    
-    
-    
+    시간 -> 초로 변환, 누적합 이용.
+    누적합이 가장 큰 재생 구간의 재생 시간 반환.
+
     */
     
-    const timeToSeconds = (time) => {
-        const [hour, minutes, seconds] = time.split(':');
+    function timeToSec(time) {
+        const [hour, minute, sec] = time.split(':').map(Number);
         
-        return Number(hour) * 3600 + Number(minutes) * 60 + Number(seconds);
+        return hour * 3600 + minute * 60 + sec;
     }
     
-    const secondsToTimes = (seconds) => {
-        let [h, m, s] = [
-            Math.floor(seconds / 3600),
-            Math.floor((seconds % 3600) / 60),
-            seconds % 60,
-        ].map(String);
-        
-        
-        return `${h.padStart(2, '0')}:${m.padStart(2, '0')}:${s.padStart(2, '0')}`;
-    }
     
-    playTime = timeToSeconds(playTime);
-    advTime = timeToSeconds(advTime);
+    const playSec = timeToSec(play_time);
+    const advSec = timeToSec(adv_time);
     
-    let prefixSum = new Array(playTime + 1).fill(0);
-    let answer = '';
+    const prefixSum = Array(playSec + 1).fill(0);
     
     for(const log of logs) {
-        let [startTime, endTime] = log.split('-');
-
-        prefixSum[timeToSeconds(startTime)]++;
-        prefixSum[timeToSeconds(endTime)]--;
+        const [sSec, eSec] = log.split('-').map(timeToSec);
+        prefixSum[sSec]++;
+        prefixSum[eSec]--;
     }
     
-    for(let i = 1; i < prefixSum.length; i++) {
-        prefixSum[i] += prefixSum[i - 1];
-    }
+    for(let i = 1; i < prefixSum.length; i++) prefixSum[i] += prefixSum[i-1];
+    for(let i = 1; i < prefixSum.length; i++) prefixSum[i] += prefixSum[i-1];
     
-    for(let i = 1; i < prefixSum.length; i++) {
-        prefixSum[i] += prefixSum[i - 1];
-    }
     
-    let max = prefixSum[advTime - 1];
-    let answerTime = null;
+    let max = prefixSum[advSec-1];
+    let answerTime = 0;
     
-    for(let i = advTime; i <= playTime; i++) {
-        const currentTime = i - advTime + 1;
-        const totalViewers = prefixSum[i] - prefixSum[currentTime - 1];
+    for(let i = advSec; i <= playSec; i++) {
+        const cur = i - advSec + 1;
+        const time = prefixSum[i] - prefixSum[cur-1];
         
-        if(totalViewers > max) {
-            answerTime = currentTime;
-            max = totalViewers;
+        if(time > max) {
+            max = time;
+            answerTime = cur;
         }
     }
     
-    return secondsToTimes(answerTime);
+    const secToTimes = (sec) => {
+        return [
+            Math.floor(sec / 3600),
+            Math.floor((sec % 3600) / 60),
+            sec % 60
+        ]
+        .map((el) => String(el).padStart(2, '0'))
+        .join(':');
+    }
+    
+    return secToTimes(answerTime);
 }
