48644d3204fc37d098d5a3d87fae3ff164c6000e
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-05-19 13:34:07 +0900

    [level 3] Title: 스타 수열, Time: 98.43 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.js
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.js
@@ -1,46 +1,34 @@
 function solution(a) {
-    /*
-    부분 수열 : 원래 수열에서 어떤 요소를 제거해서 얻을 수 있는 부분
-    스타 수열 조건
-    - 요소의 개수가 짝수
-    - 앞에서부터 두 개씩 쌍을 지었을 때 쌍들의 교집합의 원소의 개수가 1이상
-    - 단, 쌍을 이룬 두 수는 서로 다른 수여야 함
+   /*
+   부분 수열: 원소를 제거하거나 그러지 않고, '원래 순서를 유지' 하여 얻을 수 있는 '새로운 수열'
+   스타수열
+   - 길이 2 이상
+   
+   return : 가장 길이가 긴 스타 수열의 길이
+   */ 
     
-    길이가 4인 스타수열을 만드려면 5가 2개필요.
+    const hash = {};
+    let answer = 0;
     
-    풀이 전략
-    - 각 원소들의 개수를 센다.
-    - 각 원소들의 set을 순회하며 해당 원소를 교집합으로 갖도록 하는 스타수열의 길이를 갱신하며 구한다.
-    - 쌍의 개수 * 2 가 최대 스타수열의 길이이므로, 최대 길이보다 작다면 순회하지 않도록하여 효율성을 향상시킨다.
-    
-    */
-    if(a.length <= 2) return 0;
-    
-    const counts = {};
-    let maxLen = 0;
-    
-    for(const number of a) {
-        counts[number] = (counts[number] || 0) + 1;
+    for(let i = 0; i < a.length; i++) {
+        hash[a[i]] = (hash[a[i]] | 0) + 1;
     }
     
-    
-    for(const key in counts) {
-        if(counts[key] * 2 < maxLen) continue;
+    for(const v in hash) {
+        if(hash[v] <= (answer / 2)) continue;
         
-        const cur = Number(key);
-        let pairCnt = 0;
+        const V = Number(v);
+        let count = 0;
         
         for(let i = 0; i < a.length - 1; i++) {
-            const hasKey = a[i] === cur || a[i + 1] === cur;
-            const isNotEqual = a[i] !== a[i + 1];
-            
-            if(hasKey && isNotEqual) {
-                pairCnt++;
+            if((a[i] === V || a[i+1] === V) && (a[i] !== a[i+1])) {
                 i++;
+                count += 2;
             }
         }
-        maxLen = Math.max(maxLen, pairCnt * 2);
+        
+        answer = Math.max(answer, count);
     }
     
-    return maxLen;
+    return answer;
 }
