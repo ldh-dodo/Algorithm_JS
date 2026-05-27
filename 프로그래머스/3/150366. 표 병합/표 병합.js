34376c14ecf6834dd4dcbfffcf2bdbb4424c654f
Raccoon <132376178+ldh-dodo@users.noreply.github.com>
2026-05-27 15:22:54 +0900

    [level 3] Title: 표 병합, Time: 19.89 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.js
+++ 프로그래머스/3/150366. 표 병합/표 병합.js
@@ -1,106 +1,95 @@
 function solution(commands) {
-    /*
-    표 크기 : 50 x 50
-    
-    - 모든 좌표 값은 이차원이 아닌, 1차원 배열로 변환하여 사용한다.
-    - disjoint set 이용
-    
-    updateCell : 해당 좌표의 root 값을 value로 바꾼다.
-    updateValue : 모든 좌표값을 순회하며 해당 값을 가진 좌표를 목표 값으로 바꾼다.
-    merge : 같은 set 안에 집어넣는다
-    unmerge : 모든 좌표값을 순회하며 해당 좌표의 root값과 같은 root 값을 가지는 좌표들을 초기값으로 초기화
-    print : 해당 좌표의 root 값을 출력한다.
-    */
-    let answer = [];
+    // disjoint set을 사용해야함. flat 하게 표현
     const LEN = 50 * 50 + 1;
-    const cell = new Array(LEN).fill(null);
-    const p = new Array(LEN).fill().map((_, idx) => idx); // makeSet은 해당 구문으로 대체
+    const table = Array(LEN).fill(null);
+    const p = Array(LEN);
+
+    const answer = [];
     
-    const parseCoord = (u, v) => {
-        [u, v] = [Number(u), Number(v)];
-        
-        return ((u - 1) * 50) + v;
+    for(let i = 0; i < LEN; i++) {
+        p[i] = i;
     }
     
-    for(const command of commands) {
-        const com = command.split(' ');
+    const parseCoord = (r, c) => 50 * (Number(r) - 1) + Number(c);
+    
+    for(const str of commands) {
+        const strArr = str.split(' ');
+        const command = strArr[0];
+        const params = strArr.slice(1);
         
-        switch(com[0]){
-            case 'UPDATE' :
-                update(com);
-                break;
-            case 'MERGE' :
-                merge(com);
-                break;
-            case 'UNMERGE' :
-                unmerge(com);
-                break;
-            case 'PRINT' :
-                print(com);
-                break;
+        if(command === 'UPDATE') {
+            if(params.length === 3) {
+                // (r, c)가 부모로 삼는 위치의 셀의 값을 value로 변경
+                const [r, c, value] = params;
+                table[find(parseCoord(r, c))] = value;
+            } else {
+                // 표 전체를 순회하며 val1 값을 가지는 셀을 val2로 변경
+                const [val1, val2] = params;
+                
+                for(let i = 0; i < LEN; i++) {
+                    if(find(i) === i && table[i] === val1) table[i] = val2;
+                }
+            }
+        } else if(command === 'MERGE') {
+            // 같은 셀이라면 명령 무시
+            // (r1, c1) 값이 없고 (r2, c2) 값이 있다면 (r2, c2)가 (r1, c1)의 부모
+            // 이 외는 (r1, c1)가 (r2, c2)의 부모
+            const [r1, c1, r2, c2] = params;
+            
+            union(parseCoord(r1, c1), parseCoord(r2, c2));
+        } else if(command === 'UNMERGE') {
+            // (r, c)의 부모를 찾고, 테이블 전체를 순회하며 같은 부모를 가지는 셀의 부모를
+            // 자기 자신으로 되돌린다.
+            
+            // (r, c)만 병합 전 값을 가지고, 나머지는 초기값(null) 로 되돌린다.
+            const [r, c] = params;
+            
+            const u = parseCoord(r, c);
+            const root = find(u);
+            const rollbackData = table[root];
+            
+            const group = [];
+            
+            for(let i = 0; i < LEN; i++) {
+                if(find(i) === root) group.push(i);
+            }
+            
+            for(const u of group) {
+                p[u] = u;
+                table[u] = null;
+            }
+            
+            table[u] = rollbackData;
+        } else if(command === 'PRINT') {
+            // (r, c)의 부모를 찾고, 부모 셀의 값을 출력한다.
+            // 비어있다면 'EMPTY를 출력한다.'
+            
+            const [r, c] = params;
+            
+            const value = table[find(parseCoord(r, c))];
+            answer.push(value ?? "EMPTY");
         }
-    }
-    
-    function update(command) {
-        if(command[3] !== undefined) updateCell(command);
-        else updateValue(command);
-    }
-    
-    function updateCell([, r, c, val]) {
-        cell[findSet(parseCoord(r, c))] = val;
-    }
-    
-    function updateValue([, val1, val2]) {
-        for(let i = 0; i < LEN; i++) {
-            if(cell[i] === val1) cell[i] = val2;
-        }
-    }
-    
-    function merge([, r1, c1, r2, c2]) {        
-        union(parseCoord(r1, c1), parseCoord(r2, c2));
-    }
-    
-    function unmerge([, r, c]) {
-        const curCoord = parseCoord(r, c);
-        const root = findSet(curCoord);
-        const rollbackItem = cell[root];
-        const unmergeGroup = [];
         
-        for(let i = 0; i < LEN; i++) {
-            if(root === findSet(i)) unmergeGroup.push(i);
-        }
-        
-        for(const idx of unmergeGroup) {
-            p[idx] = idx;
-            cell[idx] = null;
-        }
-        
-        if(rollbackItem !== null) {
-            cell[curCoord] = rollbackItem;
-        }
     }
-    
-    function print([, r, c]) {
-        const value = cell[findSet(parseCoord(r, c))];
-        
-        answer.push(value ?? 'EMPTY');
-    }
-    
-    // disjoint set
-    
-    function findSet(u) {
-        if(p[u] !== u) p[u] = findSet(p[u]);
-        
-        return p[u];
+
+    function find(u) {
+        while(u !== p[u]) u = p[u] = p[p[u]];
+        return u;
     }
     
     function union(u, v) {
-        const [uRoot, vRoot] = [findSet(u), findSet(v)];
+        const uRoot = find(u);
+        const vRoot = find(v);
         
         if(uRoot === vRoot) return;
-        
-        if(cell[vRoot] !== null && cell[uRoot] === null) p[uRoot] = vRoot;
-        else p[vRoot] = uRoot;
+    
+        if(!table[uRoot] && table[vRoot]) {
+            // v가 u의 부모
+            p[uRoot] = vRoot;
+        } else {
+            // u가 v의 부모
+            p[vRoot] = uRoot;
+        }
     }
     
     return answer;
